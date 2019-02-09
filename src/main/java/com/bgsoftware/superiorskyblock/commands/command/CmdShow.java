b31d2e9636448c348ac2598ab742705b7974a520
OmerBenGera <omerthepro126@gmail.com>
2019-02-09 20:09:00 +0200

    Fixed info footer not showing

--- src/main/java/com/bgsoftware/superiorskyblock/commands/command/CmdShow.java
+++ src/main/java/com/bgsoftware/superiorskyblock/commands/command/CmdShow.java
@@ -121,6 +121,8 @@ public final class CmdShow implements ICommand {
         if(!Locale.ISLAND_INFO_MEMBERS.isEmpty())
             infoMessage.append(Locale.ISLAND_INFO_MEMBERS.getMessage(membersString));
 
+        infoMessage.append(Locale.ISLAND_INFO_FOOTER.getMessage());
+
         Locale.sendMessage(sender, infoMessage.toString());
     }
 
