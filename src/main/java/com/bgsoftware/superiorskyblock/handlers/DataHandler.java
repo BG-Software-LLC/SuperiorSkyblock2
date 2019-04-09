64e56f63c9bc74dada79aa1ba8e5e86fce5c9548
OmerBenGera <omerthepro126@gmail.com>
2019-04-09 16:41:36 +0300

    Fixed not suitable driver error

--- src/main/java/com/bgsoftware/superiorskyblock/handlers/DataHandler.java
+++ src/main/java/com/bgsoftware/superiorskyblock/handlers/DataHandler.java
@@ -43,6 +43,7 @@ public final class DataHandler {
         }
 
         try{
+            Class.forName("org.sqlite.JDBC");
             String sqlURL = "jdbc:sqlite:" + databaseFile.getAbsolutePath().replace("\\", "/");
             conn = DriverManager.getConnection(sqlURL);
         }catch(Exception ex){
