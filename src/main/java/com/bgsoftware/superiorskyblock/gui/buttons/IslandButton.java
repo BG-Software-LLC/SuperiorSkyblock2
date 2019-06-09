6d457ac68b58914b2bd8957cb0b10ddc96f5ab32
SirCodalot <tzubery.shoham@gmail.com>
2019-06-09 22:26:57 +0300

    Finished IslandTop Members List

--- src/main/java/com/bgsoftware/superiorskyblock/gui/buttons/IslandButton.java
+++ src/main/java/com/bgsoftware/superiorskyblock/gui/buttons/IslandButton.java
@@ -2,11 +2,16 @@ package com.bgsoftware.superiorskyblock.gui.buttons;
 
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.utils.ItemSerializer;
+import com.bgsoftware.superiorskyblock.wrappers.SSuperiorPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import java.util.function.BiConsumer;
 
 public class IslandButton extends Button{
@@ -24,6 +29,20 @@ public class IslandButton extends Button{
                 island.getWorthAsBigDecimal().toString()
         );
         SkullMeta meta = (SkullMeta) item.getItemMeta();
+
+        List<String> lore = meta.getLore();
+        List<String> newLore = new ArrayList<>();
+
+        for (String line : lore) {
+            if (!line.contains("{MEMBER}")) {
+                newLore.add(line);
+                continue;
+            }
+
+            for (UUID uuid : island.getAllMembers())
+                newLore.add(line.replace("{MEMBER}", SSuperiorPlayer.of(uuid).getName()));
+        }
+        meta.setLore(newLore);
 
         meta.setOwner(island.getOwner().getName());
         item.setItemMeta(meta);
