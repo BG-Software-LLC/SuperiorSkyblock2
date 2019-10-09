bd2f2ebb6e7ec61f6aab0db94f1b02e0b492121e
OmerBenGera <omerthepro126@gmail.com>
2019-10-09 13:02:29 +0300

    Added support to customizable roles in the member role menu

--- src/main/java/com/bgsoftware/superiorskyblock/menu/MemberRoleMenu.java
+++ src/main/java/com/bgsoftware/superiorskyblock/menu/MemberRoleMenu.java
@@ -1,23 +1,26 @@
 package com.bgsoftware.superiorskyblock.menu;
 
+import com.bgsoftware.superiorskyblock.api.island.PlayerRole;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import com.bgsoftware.superiorskyblock.utils.FileUtils;
 import com.bgsoftware.superiorskyblock.wrappers.SSuperiorPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class MemberRoleMenu extends SuperiorMenu {
 
     private static Inventory inventory = null;
     private static String title = "";
-    private static int memberSlot, modSlot, adminSlot, leaderSlot;
-    //private static Sound memberSound, modSound, adminSound, leaderSound;
+    private static Map<Integer, PlayerRole> roleSlots = new HashMap<>();
 
     private SuperiorPlayer targetPlayer;
 
@@ -31,20 +34,17 @@ public final class MemberRoleMenu extends SuperiorMenu {
         super.onClick(e);
         SuperiorPlayer superiorPlayer = SSuperiorPlayer.of(e.getWhoClicked());
 
-        if(memberSlot == e.getRawSlot()){
-            Bukkit.dispatchCommand(superiorPlayer.asPlayer(), "island setrole " + targetPlayer.getName() + " member");
-        }
+        if(!roleSlots.containsKey(e.getRawSlot()))
+            return;
 
-        else if(modSlot == e.getRawSlot()){
-            Bukkit.dispatchCommand(superiorPlayer.asPlayer(), "island setrole " + targetPlayer.getName() + " moderator");
-        }
+        PlayerRole playerRole = roleSlots.get(e.getRawSlot());
 
-        else if(adminSlot == e.getRawSlot()){
-            Bukkit.dispatchCommand(superiorPlayer.asPlayer(), "island setrole " + targetPlayer.getName() + " admin");
-        }
-
-        else if(leaderSlot == e.getRawSlot()){
+        if(playerRole.isLastRole()){
             Bukkit.dispatchCommand(superiorPlayer.asPlayer(), "island transfer " + targetPlayer.getName());
+        }
+
+        else{
+            Bukkit.dispatchCommand(superiorPlayer.asPlayer(), "island setrole " + targetPlayer.getName() + " " + playerRole.toString());
         }
     }
 
@@ -63,31 +63,23 @@ public final class MemberRoleMenu extends SuperiorMenu {
             FileUtils.saveResource("guis/panel-gui.yml");
 
         YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+        convertConfig(cfg);
 
         inventory = FileUtils.loadGUI(memberRoleMenu, cfg.getConfigurationSection("roles-panel"), 5, "");
         title = ChatColor.translateAlternateColorCodes('&', cfg.getString("roles-panel.title"));
 
-        ItemStack memberButton = FileUtils.getItemStack(cfg.getConfigurationSection("roles-panel.member-role"));
-        ItemStack modButton = FileUtils.getItemStack(cfg.getConfigurationSection("roles-panel.mod-role"));
-        ItemStack adminButton = FileUtils.getItemStack(cfg.getConfigurationSection("roles-panel.admin-role"));
-        ItemStack leaderButton = FileUtils.getItemStack(cfg.getConfigurationSection("roles-panel.leader-role"));
-        memberSlot = cfg.getInt("roles-panel.member-role.slot");
-        modSlot = cfg.getInt("roles-panel.mod-role.slot");
-        adminSlot = cfg.getInt("roles-panel.admin-role.slot");
-        leaderSlot = cfg.getInt("roles-panel.leader-role.slot");
-        memberRoleMenu.addSound(memberSlot, FileUtils.getSound(cfg.getConfigurationSection("roles-panel.member-role.sound")));
-        memberRoleMenu.addSound(modSlot, FileUtils.getSound(cfg.getConfigurationSection("roles-panel.mod-role.sound")));
-        memberRoleMenu.addSound(adminSlot, FileUtils.getSound(cfg.getConfigurationSection("roles-panel.admin-role.sound")));
-        memberRoleMenu.addSound(leaderSlot, FileUtils.getSound(cfg.getConfigurationSection("roles-panel.leader-role.sound")));
-        memberRoleMenu.addCommands(memberSlot, cfg.getStringList("roles-panel.member-role.commands"));
-        memberRoleMenu.addCommands(modSlot, cfg.getStringList("roles-panel.mod-role.commands"));
-        memberRoleMenu.addCommands(adminSlot, cfg.getStringList("roles-panel.admin-role.commands"));
-        memberRoleMenu.addCommands(leaderSlot, cfg.getStringList("roles-panel.leader-role.commands"));
+        for(PlayerRole playerRole : plugin.getPlayers().getRoles()){
+            if(cfg.contains("roles-panel.roles." + playerRole.toString().toLowerCase())){
+                ConfigurationSection roleSection = cfg.getConfigurationSection("roles-panel.roles." + playerRole.toString().toLowerCase());
+                ItemStack roleButton = FileUtils.getItemStack(roleSection);
+                int roleSlot = roleSection.getInt("slot");
+                memberRoleMenu.addSound(roleSlot, FileUtils.getSound(roleSection.getConfigurationSection("sound")));
+                memberRoleMenu.addCommands(roleSlot, cfg.getStringList("commands"));
 
-        inventory.setItem(memberSlot, memberButton);
-        inventory.setItem(modSlot, modButton);
-        inventory.setItem(adminSlot, adminButton);
-        inventory.setItem(leaderSlot, leaderButton);
+                inventory.setItem(roleSlot, roleButton);
+                roleSlots.put(roleSlot, playerRole);
+            }
+        }
     }
 
     public static SuperiorMenu getMenu(SuperiorPlayer targetPlayer){
@@ -96,6 +88,20 @@ public final class MemberRoleMenu extends SuperiorMenu {
 
     public static void openInventory(SuperiorPlayer superiorPlayer, SuperiorMenu previousMenu, SuperiorPlayer targetPlayer){
         new MemberRoleMenu(targetPlayer).open(superiorPlayer, previousMenu);
+    }
+
+    private static void convertConfig(YamlConfiguration cfg){
+        if(cfg.contains("roles-panel.member-role"))
+            cfg.set("roles-panel.roles.member", cfg.getConfigurationSection("roles-panel.member-role"));
+
+        if(cfg.contains("roles-panel.mod-role"))
+            cfg.set("roles-panel.roles.moderator", cfg.getConfigurationSection("roles-panel.mod-role"));
+
+        if(cfg.contains("roles-panel.admin-role"))
+            cfg.set("roles-panel.roles.admin", cfg.getConfigurationSection("roles-panel.admin-role"));
+
+        if(cfg.contains("roles-panel.leader-role"))
+            cfg.set("roles-panel.roles.leader", cfg.getConfigurationSection("roles-panel.leader-role"));
     }
 
 }
