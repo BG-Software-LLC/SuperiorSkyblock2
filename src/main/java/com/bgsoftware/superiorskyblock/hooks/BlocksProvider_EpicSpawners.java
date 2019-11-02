ce01ac445c9aacefc369a52f25db356ec2deffe2
OmerBenGera <omerthepro126@gmail.com>
2019-11-03 00:31:09 +0200

    Fixed the ability to exceed block limit with EpicSpawners

--- src/main/java/com/bgsoftware/superiorskyblock/hooks/BlocksProvider_EpicSpawners.java
+++ src/main/java/com/bgsoftware/superiorskyblock/hooks/BlocksProvider_EpicSpawners.java
@@ -1,9 +1,13 @@
 package com.bgsoftware.superiorskyblock.hooks;
 
+import com.bgsoftware.superiorskyblock.Locale;
 import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.island.Island;
+import com.bgsoftware.superiorskyblock.api.key.Key;
+import com.bgsoftware.superiorskyblock.island.SIsland;
 import com.bgsoftware.superiorskyblock.utils.Pair;
-import com.bgsoftware.superiorskyblock.utils.threads.Executor;
+import com.bgsoftware.superiorskyblock.utils.StringUtils;
+import com.bgsoftware.superiorskyblock.utils.legacy.Materials;
 import com.songoda.epicspawners.EpicSpawners;
 import com.songoda.epicspawners.api.events.SpawnerBreakEvent;
 import com.songoda.epicspawners.api.events.SpawnerChangeEvent;
@@ -37,21 +41,48 @@ public final class BlocksProvider_EpicSpawners implements BlocksProvider {
 
         private final SuperiorSkyblockPlugin plugin = SuperiorSkyblockPlugin.getPlugin();
 
-        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
         public void onSpawnerStack(SpawnerPlaceEvent e){
             Island island = plugin.getGrid().getIslandAt(e.getSpawner().getLocation());
-            if(island != null){
-                Executor.sync(() ->
-                        island.handleBlockPlace(e.getSpawner().getLocation().getBlock(), e.getSpawner().getFirstStack().getStackSize() - 1), 1L);
+
+            if(island == null)
+                return;
+
+            Key blockKey = Key.of(Materials.SPAWNER.toBukkitType() + ":" + e.getSpawner().getIdentifyingName().toUpperCase().replace(' ', '_'));
+
+            int blockLimit = island.getBlockLimit(blockKey), increaseAmount = e.getSpawner().getFirstStack().getStackSize() - 1;
+
+            if(blockLimit > SIsland.NO_BLOCK_LIMIT && island.getBlockCount(blockKey) + increaseAmount > blockLimit){
+                e.setCancelled(true);
+                Locale.REACHED_BLOCK_LIMIT.send(e.getPlayer(), StringUtils.format(Materials.SPAWNER.toBukkitType() + ""));
+            }
+
+            else{
+                island.handleBlockPlace(blockKey, increaseAmount);
             }
         }
 
-        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
         public void onSpawnerStack(SpawnerChangeEvent e){
             Island island = plugin.getGrid().getIslandAt(e.getSpawner().getLocation());
-            if(island != null) {
-                Executor.sync(() ->
-                        island.handleBlockPlace(e.getSpawner().getLocation().getBlock(), e.getStackSize() - e.getOldStackSize()), 1L);
+
+            if(island == null)
+                return;
+
+            Key blockKey = Key.of(Materials.SPAWNER.toBukkitType() + ":" + e.getSpawner().getIdentifyingName().toUpperCase().replace(' ', '_'));
+
+            int blockLimit = island.getBlockLimit(blockKey), increaseAmount = e.getStackSize() - e.getOldStackSize();
+
+            if(increaseAmount < 0){
+                island.handleBlockBreak(blockKey, -increaseAmount);
+            }
+            else if(blockLimit > SIsland.NO_BLOCK_LIMIT && island.getBlockCount(blockKey) + increaseAmount > blockLimit){
+                e.setCancelled(true);
+                Locale.REACHED_BLOCK_LIMIT.send(e.getPlayer(), StringUtils.format(Materials.SPAWNER.toBukkitType() + ""));
+            }
+
+            else{
+                island.handleBlockPlace(blockKey, increaseAmount);
             }
         }
 
