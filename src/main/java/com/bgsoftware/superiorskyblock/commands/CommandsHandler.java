a9f0e4e60c733dbf67ef1344010bf491d7fccee1
OmerBenGera <omerthepro126@gmail.com>
2019-11-29 17:08:57 +0200

    Optimized commands a bit

--- src/main/java/com/bgsoftware/superiorskyblock/commands/CommandsHandler.java
+++ src/main/java/com/bgsoftware/superiorskyblock/commands/CommandsHandler.java
@@ -15,6 +15,7 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -25,7 +26,9 @@ public final class CommandsHandler extends BukkitCommand {
 
     private final SuperiorSkyblockPlugin plugin;
 
-    private final List<ICommand> subCommands = new ArrayList<>();
+    private final Map<String, ICommand> subCommands = new LinkedHashMap<>();
+    private final Map<String, ICommand> aliasesToCommand = new HashMap<>();
+
     private final Map<UUID, Map<String, Long>> commandsCooldown = new HashMap<>();
 
     public CommandsHandler(SuperiorSkyblockPlugin plugin, String islandCommand){
@@ -41,104 +44,103 @@ public final class CommandsHandler extends BukkitCommand {
         instance = this;
         this.plugin = plugin;
 
-        subCommands.add(new CmdAccept());
-        subCommands.add(new CmdAdmin());
-        subCommands.add(new CmdBan());
-        subCommands.add(new CmdBiome());
-        subCommands.add(new CmdBorder());
-        subCommands.add(new CmdClose());
-        subCommands.add(new CmdCoop());
-        subCommands.add(new CmdCreate());
-        subCommands.add(new CmdDelWarp());
-        subCommands.add(new CmdDemote());
-        subCommands.add(new CmdDeposit());
-        subCommands.add(new CmdDisband());
-        subCommands.add(new CmdExpel());
-        subCommands.add(new CmdFly());
-        subCommands.add(new CmdHelp());
-        subCommands.add(new CmdInvite());
-        subCommands.add(new CmdKick());
-        subCommands.add(new CmdLeave());
-        subCommands.add(new CmdMission());
-        subCommands.add(new CmdMissions());
-        subCommands.add(new CmdName());
-        subCommands.add(new CmdOpen());
-        subCommands.add(new CmdPanel());
-        subCommands.add(new CmdPardon());
-        subCommands.add(new CmdPermissions());
-        subCommands.add(new CmdPromote());
-        subCommands.add(new CmdRankup());
-        subCommands.add(new CmdRate());
-        subCommands.add(new CmdRatings());
-        subCommands.add(new CmdRecalc());
-        subCommands.add(new CmdSetDiscord());
-        subCommands.add(new CmdSetPaypal());
-        subCommands.add(new CmdSetRole());
-        subCommands.add(new CmdSetTeleport());
-        subCommands.add(new CmdSettings());
-        subCommands.add(new CmdSetWarp());
-        subCommands.add(new CmdShow());
-        subCommands.add(new CmdTeam());
-        subCommands.add(new CmdTeamChat());
-        subCommands.add(new CmdTeleport());
-        subCommands.add(new CmdToggle());
-        subCommands.add(new CmdTop());
-        subCommands.add(new CmdTransfer());
-        subCommands.add(new CmdUncoop());
-        subCommands.add(new CmdUpgrade());
-        subCommands.add(new CmdValue());
-        subCommands.add(new CmdVisit());
-        subCommands.add(new CmdWarp());
-        subCommands.add(new CmdWarps());
-        subCommands.add(new CmdWithdraw());
+        register(new CmdAccept());
+        register(new CmdAdmin());
+        register(new CmdBan());
+        register(new CmdBiome());
+        register(new CmdBorder());
+        register(new CmdClose());
+        register(new CmdCoop());
+        register(new CmdCreate());
+        register(new CmdDelWarp());
+        register(new CmdDemote());
+        register(new CmdDeposit());
+        register(new CmdDisband());
+        register(new CmdExpel());
+        register(new CmdFly());
+        register(new CmdHelp());
+        register(new CmdInvite());
+        register(new CmdKick());
+        register(new CmdLeave());
+        register(new CmdMission());
+        register(new CmdMissions());
+        register(new CmdName());
+        register(new CmdOpen());
+        register(new CmdPanel());
+        register(new CmdPardon());
+        register(new CmdPermissions());
+        register(new CmdPromote());
+        register(new CmdRankup());
+        register(new CmdRate());
+        register(new CmdRatings());
+        register(new CmdRecalc());
+        register(new CmdSetDiscord());
+        register(new CmdSetPaypal());
+        register(new CmdSetRole());
+        register(new CmdSetTeleport());
+        register(new CmdSettings());
+        register(new CmdSetWarp());
+        register(new CmdShow());
+        register(new CmdTeam());
+        register(new CmdTeamChat());
+        register(new CmdTeleport());
+        register(new CmdToggle());
+        register(new CmdTop());
+        register(new CmdTransfer());
+        register(new CmdUncoop());
+        register(new CmdUpgrade());
+        register(new CmdValue());
+        register(new CmdVisit());
+        register(new CmdWarp());
+        register(new CmdWarps());
+        register(new CmdWithdraw());
     }
 
     @Override
     public boolean execute(CommandSender sender, String label, String[] args) {
         if(args.length > 0){
-            for(ICommand subCommand : subCommands){
-                if(subCommand.getAliases().contains(args[0].toLowerCase())){
-                    if(!(sender instanceof Player) && !subCommand.canBeExecutedByConsole()){
-                        Locale.sendMessage(sender, "&cCan be executed only by players!");
-                        return false;
-                    }
-
-                    if(!subCommand.getPermission().isEmpty() && !sender.hasPermission(subCommand.getPermission())) {
-                        Locale.NO_COMMAND_PERMISSION.send(sender);
-                        return false;
-                    }
-
-                    if(args.length < subCommand.getMinArgs() || args.length > subCommand.getMaxArgs()){
-                        Locale.COMMAND_USAGE.send(sender, getLabel() + " " + subCommand.getUsage());
-                        return false;
-                    }
-
-                    String commandLabel = subCommand.getAliases().get(0);
-
-                    if(sender instanceof Player && plugin.getSettings().commandsCooldown.containsKey(commandLabel)) {
-                        UUID uuid = ((Player) sender).getUniqueId();
-
-                        long timeToExecute = commandsCooldown.containsKey(uuid) && commandsCooldown.get(uuid).containsKey(commandLabel) ?
-                                commandsCooldown.get(uuid).get(commandLabel) : -1;
-
-                        long timeNow = System.currentTimeMillis();
-
-                        if(timeNow < timeToExecute){
-                            Locale.COMMAND_COOLDOWN_FORMAT.send(sender, StringUtils.formatTime(timeToExecute - timeNow));
-                            return false;
-                        }
-
-                        if(!commandsCooldown.containsKey(uuid)){
-                            commandsCooldown.put(uuid, new HashMap<>());
-                        }
-
-                        Map<String, Long> timedCommands = commandsCooldown.get(uuid);
-                        timedCommands.put(commandLabel, timeNow + plugin.getSettings().commandsCooldown.get(commandLabel).getKey());
-                    }
-
-                    subCommand.execute(plugin, sender, args);
+            ICommand command = getCommand(args[0]);
+            if(command != null){
+                if(!(sender instanceof Player) && !command.canBeExecutedByConsole()){
+                    Locale.sendMessage(sender, "&cCan be executed only by players!");
                     return false;
                 }
+
+                if(!command.getPermission().isEmpty() && !sender.hasPermission(command.getPermission())) {
+                    Locale.NO_COMMAND_PERMISSION.send(sender);
+                    return false;
+                }
+
+                if(args.length < command.getMinArgs() || args.length > command.getMaxArgs()){
+                    Locale.COMMAND_USAGE.send(sender, getLabel() + " " + command.getUsage());
+                    return false;
+                }
+
+                String commandLabel = command.getAliases().get(0);
+
+                if(sender instanceof Player && plugin.getSettings().commandsCooldown.containsKey(commandLabel)) {
+                    UUID uuid = ((Player) sender).getUniqueId();
+
+                    long timeToExecute = commandsCooldown.containsKey(uuid) && commandsCooldown.get(uuid).containsKey(commandLabel) ?
+                            commandsCooldown.get(uuid).get(commandLabel) : -1;
+
+                    long timeNow = System.currentTimeMillis();
+
+                    if(timeNow < timeToExecute){
+                        Locale.COMMAND_COOLDOWN_FORMAT.send(sender, StringUtils.formatTime(timeToExecute - timeNow));
+                        return false;
+                    }
+
+                    if(!commandsCooldown.containsKey(uuid)){
+                        commandsCooldown.put(uuid, new HashMap<>());
+                    }
+
+                    Map<String, Long> timedCommands = commandsCooldown.get(uuid);
+                    timedCommands.put(commandLabel, timeNow + plugin.getSettings().commandsCooldown.get(commandLabel).getKey());
+                }
+
+                command.execute(plugin, sender, args);
+                return false;
             }
         }
 
@@ -174,19 +176,16 @@ public final class CommandsHandler extends BukkitCommand {
     @Override
     public List<String> tabComplete(CommandSender sender, String label, String[] args) {
         if(args.length > 0){
-            for(ICommand subCommand : subCommands) {
-                if (subCommand.getAliases().contains(args[0].toLowerCase())){
-                    if(subCommand.getPermission() != null && !sender.hasPermission(subCommand.getPermission())){
-                        return new ArrayList<>();
-                    }
-                    return subCommand.tabComplete(plugin, sender, args);
-                }
+            ICommand command = getCommand(args[0]);
+            if(command != null){
+                return command.getPermission() != null && !sender.hasPermission(command.getPermission()) ?
+                        new ArrayList<>() : command.tabComplete(plugin, sender, args);
             }
         }
 
         List<String> list = new ArrayList<>();
 
-        for(ICommand subCommand : subCommands) {
+        for(ICommand subCommand : getSubCommands()) {
             if (subCommand.getPermission() == null || sender.hasPermission(subCommand.getPermission())) {
                 for (String aliases : subCommand.getAliases()) {
                     if (aliases.startsWith(args[0].toLowerCase())) {
@@ -200,8 +199,21 @@ public final class CommandsHandler extends BukkitCommand {
         return list;
     }
 
+    private void register(ICommand cmd){
+        List<String> aliases = cmd.getAliases();
+        subCommands.put(aliases.get(0).toLowerCase(), cmd);
+        for(int i = 1; i < aliases.size(); i++){
+            aliasesToCommand.put(aliases.get(i).toLowerCase(), cmd);
+        }
+    }
+
+    private ICommand getCommand(String label){
+        label = label.toLowerCase();
+        return subCommands.getOrDefault(label, aliasesToCommand.get(label));
+    }
+
     public static List<ICommand> getSubCommands(){
-        return instance.subCommands;
+        return new ArrayList<>(instance.subCommands.values());
     }
 
     public static String getCommandLabel(){
