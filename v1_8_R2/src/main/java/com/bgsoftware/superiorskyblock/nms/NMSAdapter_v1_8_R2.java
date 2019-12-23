38d89daf2635742a44c9aebecd1e634019a9690f
OmerBenGera <omerthepro126@gmail.com>
2019-12-23 21:23:23 +0200

    Added support for 1_8_R2

--- v1_8_R2/src/main/java/com/bgsoftware/superiorskyblock/nms/NMSAdapter_v1_8_R2.java
+++ v1_8_R2/src/main/java/com/bgsoftware/superiorskyblock/nms/NMSAdapter_v1_8_R2.java
@@ -1,20 +1,24 @@
 package com.bgsoftware.superiorskyblock.nms;
 
+import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.key.Key;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
-import com.bgsoftware.superiorskyblock.utils.key.SKey;
-import com.mojang.authlib.properties.Property;
-import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.utils.tags.CompoundTag;
+import com.bgsoftware.superiorskyblock.utils.tags.ListTag;
+import com.bgsoftware.superiorskyblock.utils.tags.Tag;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import net.minecraft.server.v1_8_R2.Block;
 import net.minecraft.server.v1_8_R2.BlockPosition;
 import net.minecraft.server.v1_8_R2.Chunk;
-import net.minecraft.server.v1_8_R2.EntityHuman;
 import net.minecraft.server.v1_8_R2.EntityLiving;
 import net.minecraft.server.v1_8_R2.EntityPlayer;
+import net.minecraft.server.v1_8_R2.EnumParticle;
 import net.minecraft.server.v1_8_R2.IBlockData;
 import net.minecraft.server.v1_8_R2.ItemStack;
+import net.minecraft.server.v1_8_R2.MinecraftServer;
+import net.minecraft.server.v1_8_R2.NBTBase;
 import net.minecraft.server.v1_8_R2.NBTTagByte;
 import net.minecraft.server.v1_8_R2.NBTTagByteArray;
 import net.minecraft.server.v1_8_R2.NBTTagCompound;
@@ -28,21 +32,26 @@ import net.minecraft.server.v1_8_R2.NBTTagShort;
 import net.minecraft.server.v1_8_R2.NBTTagString;
 import net.minecraft.server.v1_8_R2.PacketPlayOutMapChunk;
 import net.minecraft.server.v1_8_R2.PacketPlayOutWorldBorder;
+import net.minecraft.server.v1_8_R2.PlayerInteractManager;
 import net.minecraft.server.v1_8_R2.TileEntityFlowerPot;
 import net.minecraft.server.v1_8_R2.TileEntityMobSpawner;
 import net.minecraft.server.v1_8_R2.World;
-
 import net.minecraft.server.v1_8_R2.WorldBorder;
+import net.minecraft.server.v1_8_R2.WorldServer;
+import org.bukkit.Bukkit;
 import org.bukkit.ChunkSnapshot;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.craftbukkit.v1_8_R2.CraftChunk;
+import org.bukkit.craftbukkit.v1_8_R2.CraftServer;
 import org.bukkit.craftbukkit.v1_8_R2.CraftWorld;
-import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R2.entity.CraftLivingEntity;
 import org.bukkit.craftbukkit.v1_8_R2.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_8_R2.inventory.CraftItemStack;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 
 import java.util.Optional;
 import java.util.Set;
@@ -105,6 +114,8 @@ public final class NMSAdapter_v1_8_R2 implements NMSAdapter {
         EntityLiving entityLiving = ((CraftLivingEntity) livingEntity).getHandle();
         NBTTagCompound nbtTagCompound = new NBTTagCompound();
         entityLiving.b(nbtTagCompound);
+        nbtTagCompound.set("Yaw", new NBTTagFloat(entityLiving.yaw));
+        nbtTagCompound.set("Pitch", new NBTTagFloat(entityLiving.pitch));
         return CompoundTag.fromNBT(nbtTagCompound);
     }
 
@@ -112,8 +123,16 @@ public final class NMSAdapter_v1_8_R2 implements NMSAdapter {
     public void getFromNBTTag(LivingEntity livingEntity, CompoundTag compoundTag) {
         EntityLiving entityLiving = ((CraftLivingEntity) livingEntity).getHandle();
         NBTTagCompound nbtTagCompound = (NBTTagCompound) compoundTag.toNBT();
-        if(nbtTagCompound != null)
+        if(nbtTagCompound != null) {
             entityLiving.a(nbtTagCompound);
+            if(nbtTagCompound.hasKey("Yaw") && nbtTagCompound.hasKey("Pitch")){
+                entityLiving.setLocation(
+                        entityLiving.locX, entityLiving.locY, entityLiving.locZ,
+                        nbtTagCompound.getFloat("Yaw"),
+                        nbtTagCompound.getFloat("Pitch")
+                );
+            }
+        }
     }
 
     @Override
@@ -121,7 +140,7 @@ public final class NMSAdapter_v1_8_R2 implements NMSAdapter {
     public Key getBlockKey(ChunkSnapshot chunkSnapshot, int x, int y, int z) {
         Material type = Material.getMaterial(chunkSnapshot.getBlockTypeId(x, y, z));
         short data = (short) chunkSnapshot.getBlockData(x, y, z);
-        return SKey.of(type, data);
+        return Key.of(type, data);
     }
 
     @Override
@@ -136,32 +155,43 @@ public final class NMSAdapter_v1_8_R2 implements NMSAdapter {
     public void refreshChunk(org.bukkit.Chunk bukkitChunk) {
         World world = ((CraftWorld) bukkitChunk.getWorld()).getHandle();
         Chunk chunk = ((CraftChunk) bukkitChunk).getHandle();
-        for(EntityHuman entityHuman : world.players)
-            ((EntityPlayer) entityHuman).playerConnection.sendPacket(new PacketPlayOutMapChunk(chunk, true, 65535));
+        for(Object object : world.players)
+            ((EntityPlayer) object).playerConnection.sendPacket(new PacketPlayOutMapChunk(chunk, true, 65535));
     }
 
     @Override
     public void setWorldBorder(SuperiorPlayer superiorPlayer, Island island) {
-        if(!plugin.getSettings().worldBordersEnabled)
-            return;
+        try {
+            if(!plugin.getSettings().worldBordersEnabled)
+                return;
 
-        boolean disabled = !superiorPlayer.hasWorldBorderEnabled();
+            boolean disabled = !superiorPlayer.hasWorldBorderEnabled();
 
-        WorldBorder worldBorder = new WorldBorder();
+            WorldBorder worldBorder = new WorldBorder();
 
-        worldBorder.world = ((CraftWorld) superiorPlayer.getWorld()).getHandle();
-        worldBorder.setSize(disabled || island == null ? Integer.MAX_VALUE : (island.getIslandSize() * 2) + 1);
+            worldBorder.world = ((CraftWorld) superiorPlayer.getWorld()).getHandle();
+            worldBorder.setSize(disabled || island == null || (!plugin.getSettings().spawnWorldBorder && island.isSpawn()) ? Integer.MAX_VALUE : (island.getIslandSize() * 2) + 1);
 
-        Location center = island == null ? superiorPlayer.getLocation() : island.getCenter();
+            Location center = island == null ? superiorPlayer.getLocation() : island.getCenter();
 
-        if (superiorPlayer.getWorld().getEnvironment() == org.bukkit.World.Environment.NETHER) {
-            worldBorder.setCenter(center.getX() * 8, center.getZ() * 8);
-        } else {
-            worldBorder.setCenter(center.getX(), center.getZ());
-        }
+            if (superiorPlayer.getWorld().getEnvironment() == org.bukkit.World.Environment.NETHER) {
+                worldBorder.setCenter(center.getX() * 8, center.getZ() * 8);
+            } else {
+                worldBorder.setCenter(center.getX(), center.getZ());
+            }
 
-        PacketPlayOutWorldBorder packetPlayOutWorldBorder = new PacketPlayOutWorldBorder(worldBorder, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE);
-        ((CraftPlayer) superiorPlayer.asPlayer()).getHandle().playerConnection.sendPacket(packetPlayOutWorldBorder);
+            switch (superiorPlayer.getBorderColor()){
+                case GREEN:
+                    worldBorder.transitionSizeBetween(worldBorder.getSize() - 0.1D, worldBorder.getSize(), Long.MAX_VALUE);
+                    break;
+                case RED:
+                    worldBorder.transitionSizeBetween(worldBorder.getSize(), worldBorder.getSize() - 1.0D, Long.MAX_VALUE);
+                    break;
+            }
+
+            PacketPlayOutWorldBorder packetPlayOutWorldBorder = new PacketPlayOutWorldBorder(worldBorder, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE);
+            ((CraftPlayer) superiorPlayer.asPlayer()).getHandle().playerConnection.sendPacket(packetPlayOutWorldBorder);
+        } catch (NullPointerException ignored) {}
     }
 
     @Override
@@ -226,4 +256,51 @@ public final class NMSAdapter_v1_8_R2 implements NMSAdapter {
         return ((NBTTagString) object).a_();
     }
 
+    @Override
+    public Object parseList(ListTag listTag) {
+        NBTTagList nbtTagList = new NBTTagList();
+
+        for(Tag tag : listTag.getValue())
+            nbtTagList.add((NBTBase) tag.toNBT());
+
+        return nbtTagList;
+    }
+
+    @Override
+    public void clearInventory(OfflinePlayer offlinePlayer) {
+        if(offlinePlayer.isOnline() || offlinePlayer instanceof Player){
+            Player player = offlinePlayer instanceof Player ? (Player) offlinePlayer : offlinePlayer.getPlayer();
+            player.getInventory().clear();
+            player.getEnderChest().clear();
+            return;
+        }
+
+        GameProfile profile = new GameProfile(offlinePlayer.getUniqueId(), offlinePlayer.getName());
+
+        MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer();
+        WorldServer worldServer = server.getWorldServer(0);
+        EntityPlayer entity = new EntityPlayer(server, worldServer, profile, new PlayerInteractManager(worldServer));
+        Player targetPlayer = entity.getBukkitEntity();
+
+        targetPlayer.loadData();
+
+        clearInventory(targetPlayer);
+
+        //Setting the entity to the spawn location
+        Location spawnLocation = plugin.getGrid().getSpawnIsland().getCenter();
+        entity.world = ((CraftWorld) spawnLocation.getWorld()).getHandle();
+        entity.setPositionRotation(spawnLocation.getX(), spawnLocation.getY(), spawnLocation.getZ(), spawnLocation.getYaw(), spawnLocation.getPitch());
+
+        targetPlayer.saveData();
+    }
+
+    @Override
+    public void playGeneratorSound(Location location) {
+        World world = ((CraftWorld) location.getWorld()).getHandle();
+        double x = location.getX(), y = location.getY(), z = location.getZ();
+        world.makeSound(x + 0.5D, y + 0.5D, z + 0.5D, "random.fizz", 0.5F, 2.6F + (world.random.nextFloat() - world.random.nextFloat()) * 0.8F);
+
+        for(int i = 0; i < 8; i++)
+            world.addParticle(EnumParticle.SMOKE_LARGE, x + Math.random(), y + 1.2D, z + Math.random(), 0.0D, 0.0D, 0.0D);
+    }
 }
