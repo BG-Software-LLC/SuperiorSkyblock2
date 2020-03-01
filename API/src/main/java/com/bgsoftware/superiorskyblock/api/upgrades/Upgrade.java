563c366ad4a610bc8dfbd07e22862711493ec333
OmerBenGera <omerthepro126@gmail.com>
2020-03-01 14:28:08 +0200

    Commented upgrade objects

--- API/src/main/java/com/bgsoftware/superiorskyblock/api/upgrades/Upgrade.java
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/upgrades/Upgrade.java
@@ -2,10 +2,21 @@ package com.bgsoftware.superiorskyblock.api.upgrades;
 
 public interface Upgrade {
 
+    /**
+     * Get the name of the upgrade.
+     */
     String getName();
 
+    /**
+     * Get the upgrade-level object from a level.
+     * If doesn't exist, null will be returned.
+     * @param level The level to get the object from.
+     */
     UpgradeLevel getUpgradeLevel(int level);
 
+    /**
+     * Get the maximum level that exists for the upgrade.
+     */
     int getMaxUpgradeLevel();
 
 }
--- API/src/main/java/com/bgsoftware/superiorskyblock/api/upgrades/UpgradeLevel.java
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/upgrades/UpgradeLevel.java
@@ -7,32 +7,77 @@ import java.util.Map;
 
 public interface UpgradeLevel {
 
+    /**
+     * Get the level of the current upgrade-level.
+     */
     int getLevel();
 
+    /**
+     * Get the price required to upgrade to the next level.
+     */
     double getPrice();
 
+    /**
+     * Get all commands that will be executed when upgrading to the next level.
+     */
     List<String> getCommands();
 
+    /**
+     * Get the permission required to upgrade to this level.
+     */
     String getPermission();
 
+    /**
+     * Get the crop growth multiplier of this level.
+     */
     double getCropGrowth();
 
+    /**
+     * Get the spawner rates multiplier of this level.
+     */
     double getSpawnerRates();
 
+    /**
+     * Get the mob drops multiplier of this level.
+     */
     double getMobDrops();
 
+    /**
+     * Get the limit of a block for this level.
+     * @param key The block to check.
+     */
     int getBlockLimit(Key key);
 
+    /**
+     * Get the exact limit of a block for this level.
+     * @param key The block to check.
+     */
     int getExactBlockLimit(Key key);
 
+    /**
+     * Get the team limit of this level.
+     */
     int getTeamLimit();
 
+    /**
+     * Get the warps limit of this level.
+     */
     int getWarpsLimit();
 
+    /**
+     * Get the border size of this level.
+     */
     int getBorderSize();
 
+    /**
+     * Get the generator rate of a block for this level.
+     * @param key The block to check.
+     */
     int getGeneratorAmount(Key key);
 
+    /**
+     * Get all the generator rates for this level.
+     */
     Map<String, Integer> getGeneratorAmounts();
 
 }
