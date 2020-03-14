78c881665e97a5e0d26e9e7e67fc34261c10b8df
OmerBenGera <omerthepro126@gmail.com>
2020-03-14 21:45:19 +0200

    Fixed position placeholders in chat

--- src/main/java/com/bgsoftware/superiorskyblock/listeners/PlayersListener.java
+++ src/main/java/com/bgsoftware/superiorskyblock/listeners/PlayersListener.java
@@ -364,10 +364,10 @@ public final class PlayersListener implements Listener {
                 .replace("{island-worth}", String.valueOf(island == null ? 0 : island.getWorth()))
                 .replace("{island-worth-format}", String.valueOf(island == null ? 0 : StringUtils.fancyFormat(island.getWorth())))
                 .replace("{island-name}", islandNameFormat == null ? "" : islandNameFormat)
-                .replace("{island-position-worth}", island == null ? "" : plugin.getGrid().getIslandPosition(island, SortingTypes.BY_WORTH) + "")
-                .replace("{island-position-level}", island == null ? "" : plugin.getGrid().getIslandPosition(island, SortingTypes.BY_WORTH) + "")
-                .replace("{island-position-rating}", island == null ? "" : plugin.getGrid().getIslandPosition(island, SortingTypes.BY_WORTH) + "")
-                .replace("{island-position-players}", island == null ? "" : plugin.getGrid().getIslandPosition(island, SortingTypes.BY_WORTH) + "")
+                .replace("{island-position-worth}", island == null ? "" : (plugin.getGrid().getIslandPosition(island, SortingTypes.BY_WORTH) + 1) + "")
+                .replace("{island-position-level}", island == null ? "" : (plugin.getGrid().getIslandPosition(island, SortingTypes.BY_LEVEL) + 1) + "")
+                .replace("{island-position-rating}", island == null ? "" : (plugin.getGrid().getIslandPosition(island, SortingTypes.BY_RATING) + 1) + "")
+                .replace("{island-position-players}", island == null ? "" : (plugin.getGrid().getIslandPosition(island, SortingTypes.BY_PLAYERS) + 1) + "")
         );
     }
 
