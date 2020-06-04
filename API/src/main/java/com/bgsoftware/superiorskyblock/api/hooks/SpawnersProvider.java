fd20916da568cbadc0798ab574f67c3a05ccd8fd
OmerBenGera <omerthepro126@gmail.com>
2020-06-04 21:41:40 +0300

    Commented SpawnersProvider

--- API/src/main/java/com/bgsoftware/superiorskyblock/api/hooks/SpawnersProvider.java
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/hooks/SpawnersProvider.java
@@ -7,8 +7,18 @@ import org.bukkit.inventory.ItemStack;
 
 public interface SpawnersProvider {
 
+    /**
+     * Get a pair that represents information about a spawner in a specific location.
+     * This method is called async first, and if the string in the pair is null, it will be called synced later.
+     * The integer represents the amount of spawners in that location, and the string represents the entity type.
+     * @param location The location to check.
+     */
     Pair<Integer, String> getSpawner(Location location);
 
+    /**
+     * Get the spawner type from an item.
+     * @param itemStack The item to check.
+     */
     String getSpawnerType(ItemStack itemStack);
 
 }
