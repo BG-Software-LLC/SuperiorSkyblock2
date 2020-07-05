cfcdd666e3b907fedd4f8068f15a1c44f6630aed
OmerBenGera <omerthepro126@gmail.com>
2020-07-06 00:13:36 +0300

    Commented some methods

--- API/src/main/java/com/bgsoftware/superiorskyblock/api/key/CustomKeyParser.java
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/key/CustomKeyParser.java
@@ -4,8 +4,17 @@ import org.bukkit.Location;
 
 public interface CustomKeyParser {
 
+    /**
+     * Get a custom key for a block.
+     * Please note: this method should support async calls.
+     * @param location The location of the block.
+     */
     Key getCustomKey(Location location);
 
+    /**
+     * Check if a key was created by this parser.
+     * @param key The key to check.
+     */
     boolean isCustomKey(Key key);
 
 }
