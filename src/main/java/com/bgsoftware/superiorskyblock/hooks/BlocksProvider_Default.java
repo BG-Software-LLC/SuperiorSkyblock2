a32c487f00bf44c4072507f7d74d16939511b1bc
OmerBenGera <omerthepro126@gmail.com>
2020-07-12 17:26:20 +0300

    Cleaned code

--- src/main/java/com/bgsoftware/superiorskyblock/hooks/BlocksProvider_Default.java
+++ src/main/java/com/bgsoftware/superiorskyblock/hooks/BlocksProvider_Default.java
@@ -4,7 +4,7 @@ import com.bgsoftware.superiorskyblock.api.objects.Pair;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 
-public class BlocksProvider_Default implements BlocksProvider {
+public final class BlocksProvider_Default implements BlocksProvider {
 
     @Override
     public Pair<Integer, String> getSpawner(Location location) {
