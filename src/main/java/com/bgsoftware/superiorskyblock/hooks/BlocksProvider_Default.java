00a20531c05a56230b0ed610f89e8a8f01811859
OmerBenGera <omerthepro126@gmail.com>
2020-07-12 17:26:41 +0300

    Cleaned code

--- src/main/java/com/bgsoftware/superiorskyblock/hooks/BlocksProvider_Default.java
+++ src/main/java/com/bgsoftware/superiorskyblock/hooks/BlocksProvider_Default.java
@@ -2,7 +2,6 @@ package com.bgsoftware.superiorskyblock.hooks;
 
 import com.bgsoftware.superiorskyblock.api.objects.Pair;
 import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
 
 public final class BlocksProvider_Default implements BlocksProvider {
 
