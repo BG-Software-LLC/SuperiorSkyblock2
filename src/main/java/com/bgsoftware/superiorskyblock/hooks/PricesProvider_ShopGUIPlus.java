fa895410b85be229d280a36e3cf505c46855f1b8
OmerBenGera <omerthepro126@gmail.com>
2020-09-28 12:07:29 +0300

    Added a caching system for prices of ShopGUIPlus

--- src/main/java/com/bgsoftware/superiorskyblock/hooks/PricesProvider_ShopGUIPlus.java
+++ src/main/java/com/bgsoftware/superiorskyblock/hooks/PricesProvider_ShopGUIPlus.java
@@ -2,44 +2,51 @@ package com.bgsoftware.superiorskyblock.hooks;
 
 import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.key.Key;
+import com.bgsoftware.superiorskyblock.utils.key.KeyMap;
 import net.brcdev.shopgui.ShopGuiPlugin;
 import net.brcdev.shopgui.shop.Shop;
 import net.brcdev.shopgui.shop.ShopItem;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class PricesProvider_ShopGUIPlus implements PricesProvider{
 
     private static final SuperiorSkyblockPlugin plugin = SuperiorSkyblockPlugin.getPlugin();
     private static final ShopGuiPlugin shopPlugin = ShopGuiPlugin.getInstance();
+    private static final KeyMap<Double> cachedPrices = new KeyMap<>();
 
     @Override
     public BigDecimal getPrice(Key key) {
-        double price = 0;
+        double price = cachedPrices.getOrDefault(key, 0D);
 
-        Map<String, Shop> shops = shopPlugin.getShopManager().shops;
-        for(Shop shop : shops.values()){
-            for(ShopItem shopItem : shop.getShopItems()){
-                if(Key.of(shopItem.getItem()).equals(key)){
-                    double shopPrice;
+        if(price == 0) {
+            Map<String, Shop> shops = new HashMap<>(shopPlugin.getShopManager().shops);
+            for (Shop shop : shops.values()) {
+                for (ShopItem shopItem : shop.getShopItems()) {
+                    if (Key.of(shopItem.getItem()).equals(key)) {
+                        double shopPrice;
 
-                    switch (plugin.getSettings().syncWorth){
-                        case BUY:
-                            //noinspection deprecation
-                            shopPrice = shopItem.getBuyPriceForAmount(1);
-                            break;
-                        case SELL:
-                            //noinspection deprecation
-                            shopPrice = shopItem.getSellPriceForAmount(1);
-                            break;
-                        default:
-                            shopPrice = 0;
-                            break;
+                        switch (plugin.getSettings().syncWorth) {
+                            case BUY:
+                                //noinspection deprecation
+                                shopPrice = shopItem.getBuyPriceForAmount(1);
+                                break;
+                            case SELL:
+                                //noinspection deprecation
+                                shopPrice = shopItem.getSellPriceForAmount(1);
+                                break;
+                            default:
+                                shopPrice = 0;
+                                break;
+                        }
+
+                        if (shopPrice > price) {
+                            price = shopPrice;
+                            cachedPrices.put(key, price);
+                        }
                     }
-
-                    if(shopPrice > price)
-                        price = shopPrice;
                 }
             }
         }
