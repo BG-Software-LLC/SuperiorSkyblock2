026a0894b61a1f505826c6cc29b361706c7cc968
OmerBenGera <omerthepro126@gmail.com>
2020-09-29 14:48:24 +0300

    Added support for Android miners from Slimefun

--- src/main/java/com/bgsoftware/superiorskyblock/hooks/SlimefunHook.java
+++ src/main/java/com/bgsoftware/superiorskyblock/hooks/SlimefunHook.java
@@ -4,23 +4,29 @@ import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.island.IslandPrivilege;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
+import com.bgsoftware.superiorskyblock.listeners.BlocksListener;
 import com.bgsoftware.superiorskyblock.utils.islands.IslandFlags;
 import com.bgsoftware.superiorskyblock.utils.islands.IslandPrivileges;
 import com.bgsoftware.superiorskyblock.utils.threads.Executor;
+import io.github.thebusybiscuit.slimefun4.api.events.AndroidMineEvent;
 import me.mrCookieSlime.Slimefun.cscorelib2.protection.ProtectableAction;
 import me.mrCookieSlime.Slimefun.cscorelib2.protection.ProtectionManager;
 import me.mrCookieSlime.Slimefun.cscorelib2.protection.ProtectionModule;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
-public final class SlimefunHook implements ProtectionModule {
+public final class SlimefunHook implements ProtectionModule, Listener {
 
     private final SuperiorSkyblockPlugin plugin;
 
     private SlimefunHook(SuperiorSkyblockPlugin plugin){
         this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
@@ -59,6 +65,15 @@ public final class SlimefunHook implements ProtectionModule {
         }
 
         return islandPrivilege != null && island != null && island.hasPermission(superiorPlayer, islandPrivilege);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onAndroidMiner(AndroidMineEvent e){
+        SuperiorSkyblockPlugin.debug("Action: Android Break, Block: " + e.getBlock().getLocation() + ", Type: " + e.getBlock().getType());
+        if(BlocksListener.tryUnstack(null, e.getBlock(), plugin))
+            e.setCancelled(true);
+        else
+            BlocksListener.handleBlockBreak(plugin, e.getBlock());
     }
 
     public static void register(SuperiorSkyblockPlugin plugin){
