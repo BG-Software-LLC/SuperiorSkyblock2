394ae6c43ba6be18f567793c72a194008e9593c3
oop-778 <oskardhavel@gmail.com>
2021-03-15 21:03:11 +0200

    Created PlaceholderUpgradeCost & PlaceholderUpgradeCostProvider

---
+++ src/main/java/com/bgsoftware/superiorskyblock/upgrades/providers/placeholder/PlaceholderUpgradeCost.java
@@ -0,0 +1,36 @@
+package com.bgsoftware.superiorskyblock.upgrades.providers.placeholder;
+
+import com.bgsoftware.superiorskyblock.api.upgrades.UpgradeCost;
+import com.bgsoftware.superiorskyblock.api.upgrades.UpgradeCostProvider;
+import com.bgsoftware.superiorskyblock.upgrades.SUpgradeCost;
+
+import java.math.BigDecimal;
+
+public class PlaceholderUpgradeCost extends SUpgradeCost {
+
+    private final String placeholder;
+    private final String takeCommand;
+    public PlaceholderUpgradeCost(BigDecimal value, String placeholder, String takeCommand, UpgradeCostProvider provider) {
+        super(value, provider);
+        this.placeholder = placeholder;
+        this.takeCommand = takeCommand;
+    }
+
+    public String getPlaceholder() {
+        return placeholder;
+    }
+
+    public String getTakeCommand() {
+        return takeCommand;
+    }
+
+    @Override
+    public UpgradeCost copyOfValue(BigDecimal bigDecimal) {
+        return new PlaceholderUpgradeCost(
+                bigDecimal,
+                placeholder,
+                takeCommand,
+                getProvider()
+        );
+    }
+}
