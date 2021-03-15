34f07d4d7ee17c04a4b65a2ee8c2707c93efa6c0
oop-778 <oskardhavel@gmail.com>
2021-03-15 21:02:07 +0200

    Made so each UpgradeCostProvider can create own UpgradeCost

--- API/src/main/java/com/bgsoftware/superiorskyblock/api/upgrades/UpgradeCostProvider.java
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/upgrades/UpgradeCostProvider.java
@@ -1,6 +1,8 @@
 package com.bgsoftware.superiorskyblock.api.upgrades;
 
+import com.bgsoftware.superiorskyblock.api.objects.Pair;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
+import org.bukkit.configuration.ConfigurationSection;
 
 import java.math.BigDecimal;
 
@@ -13,13 +15,21 @@ public interface UpgradeCostProvider {
     /**
      * Get the amount of money a specific user has in his bank.
      * @param superiorPlayer The player to check.
+     * @param cost from what to get the balance
      */
-    BigDecimal getBalance(SuperiorPlayer superiorPlayer);
+    BigDecimal getBalance(SuperiorPlayer superiorPlayer, UpgradeCost cost);
 
     /**
      * take specified amount of the player
      * @param superiorPlayer the player to take from
-     * @param amount amount to take
+     * @param cost amount to take
      */
-    void take(SuperiorPlayer superiorPlayer, BigDecimal amount);
+    void take(SuperiorPlayer superiorPlayer, UpgradeCost cost);
+
+    /**
+     * Create upgrade cost from level section
+     * @param from level section
+     * @return Pair of UpgradeCost or if missing any values message with null as key
+     */
+    Pair<UpgradeCost, String> createCost(ConfigurationSection from);
 }
