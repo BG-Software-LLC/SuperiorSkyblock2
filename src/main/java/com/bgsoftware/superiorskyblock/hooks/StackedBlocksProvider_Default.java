de064876533be72fe17f52a9822df556e5a6e327
OmerBenGera <omerthepro126@gmail.com>
2021-08-17 17:15:43 +0300

    Fixed error with default StackedBlocksProvider_Default causing calculation to not work (#475)

--- src/main/java/com/bgsoftware/superiorskyblock/hooks/StackedBlocksProvider_Default.java
+++ src/main/java/com/bgsoftware/superiorskyblock/hooks/StackedBlocksProvider_Default.java
@@ -1,23 +1,17 @@
 package com.bgsoftware.superiorskyblock.hooks;
 
-import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.key.Key;
 import com.bgsoftware.superiorskyblock.api.objects.Pair;
-import com.bgsoftware.superiorskyblock.utils.chunks.ChunkPosition;
 import org.bukkit.World;
 
 import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 public final class StackedBlocksProvider_Default implements StackedBlocksProvider_AutoDetect {
 
-    private static final SuperiorSkyblockPlugin plugin = SuperiorSkyblockPlugin.getPlugin();
-
     @Override
     public Collection<Pair<Key, Integer>> getBlocks(World world, int chunkX, int chunkZ) {
-        return plugin.getGrid().getStackedBlocks(ChunkPosition.of(world, chunkX, chunkZ)).stream()
-                .map(stackedBlock -> new Pair<>((Key) stackedBlock.getBlockKey(), stackedBlock.getAmount()))
-                .collect(Collectors.toSet());
+        return Collections.emptyList();
     }
 
 }
