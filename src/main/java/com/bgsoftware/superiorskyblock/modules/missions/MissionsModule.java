9499dba3bd6577bba55a4cffddf3a90be48a2a25
OmerBenGera <omerthepro126@gmail.com>
2021-08-29 00:47:53 +0300

    Added slayer missions

--- src/main/java/com/bgsoftware/superiorskyblock/modules/missions/MissionsModule.java
+++ src/main/java/com/bgsoftware/superiorskyblock/modules/missions/MissionsModule.java
@@ -113,6 +113,10 @@ public final class MissionsModule extends BuiltinModule {
             FileUtils.saveResource("modules/missions/categories/farmer/farmer_3.yml");
             FileUtils.saveResource("modules/missions/categories/farmer/farmer_4.yml");
             FileUtils.saveResource("modules/missions/categories/farmer/farmer_5.yml");
+            FileUtils.saveResource("modules/missions/categories/slayer/slayer_1.yml");
+            FileUtils.saveResource("modules/missions/categories/slayer/slayer_2.yml");
+            FileUtils.saveResource("modules/missions/categories/slayer/slayer_3.yml");
+            FileUtils.saveResource("modules/missions/categories/slayer/slayer_4.yml");
         }
     }
 
