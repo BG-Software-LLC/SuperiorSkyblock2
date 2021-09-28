e458f9d2569e8f83a687b52e05341c2daf325bf6
OmerBenGera <omerthepro126@gmail.com>
2021-09-28 14:00:42 +0300

    Fixed visitors-blocked-commands affecting coop players (#626)

--- src/main/java/com/bgsoftware/superiorskyblock/listeners/PlayersListener.java
+++ src/main/java/com/bgsoftware/superiorskyblock/listeners/PlayersListener.java
@@ -509,7 +509,7 @@ public final class PlayersListener implements Listener {
         Island island = plugin.getGrid().getIslandAt(e.getPlayer().getLocation());
 
         String message = e.getMessage().toLowerCase();
-        if(island != null && !island.isSpawn() && island.isVisitor(superiorPlayer, false) &&
+        if(island != null && !island.isSpawn() && island.isVisitor(superiorPlayer, true) &&
                 plugin.getSettings().getBlockedVisitorsCommands().stream().anyMatch(message::contains)){
             e.setCancelled(true);
             Locale.VISITOR_BLOCK_COMMAND.send(superiorPlayer);
