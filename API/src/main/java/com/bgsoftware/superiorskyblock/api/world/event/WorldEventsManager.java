e4924251522d009fb9f96e7b2af845ada8725a9d
OmerBenGera <omerthepro126@gmail.com>
2021-10-02 15:16:19 +0300

    Moved chunks loading/unloading logic to its own package

---
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/world/event/WorldEventsManager.java
@@ -0,0 +1,19 @@
+package com.bgsoftware.superiorskyblock.api.world.event;
+
+import org.bukkit.Chunk;
+
+public interface WorldEventsManager {
+
+    /**
+     * Handles a new chunk being loaded.
+     * @param chunk The chunk that was loaded.
+     */
+    void loadChunk(Chunk chunk);
+
+    /**
+     * Handles a chunk being unloaded.
+     * @param chunk The chunk that was unloaded.
+     */
+    void unloadChunk(Chunk chunk);
+
+}
--- src/main/java/com/bgsoftware/superiorskyblock/listeners/ChunksListener.java
+++ src/main/java/com/bgsoftware/superiorskyblock/listeners/ChunksListener.java
@@ -1,12 +1,7 @@
 package com.bgsoftware.superiorskyblock.listeners;
 
-import com.bgsoftware.common.reflection.ReflectMethod;
 import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.island.Island;
-import com.bgsoftware.superiorskyblock.utils.chunks.ChunksTracker;
-import org.bukkit.Location;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,8 +12,6 @@ import org.bukkit.event.world.ChunkUnloadEvent;
 @SuppressWarnings("unused")
 public final class ChunksListener implements Listener {
 
-    private static final ReflectMethod<Void> SET_SAVE_CHUNK = new ReflectMethod<>(ChunkUnloadEvent.class, "setSaveChunk", boolean.class);
-
     private final SuperiorSkyblockPlugin plugin;
 
     public ChunksListener(SuperiorSkyblockPlugin plugin) {
@@ -27,50 +20,12 @@ public final class ChunksListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onChunkUnloadMonitor(ChunkUnloadEvent e) {
-        if (!plugin.getGrid().isIslandsWorld(e.getWorld()))
-            return;
-
-        plugin.getStackedBlocks().removeStackedBlockHolograms(e.getChunk());
-
-        Island island = plugin.getGrid().getIslandAt(e.getChunk());
-
-        if (island == null)
-            return;
-
-        plugin.getNMSChunks().startTickingChunk(island, e.getChunk(), true);
-
-        if (!island.isSpawn() && !plugin.getNMSChunks().isChunkEmpty(e.getChunk()))
-            ChunksTracker.markDirty(island, e.getChunk(), true);
+        plugin.getWorldEventsManager().unloadChunk(e.getChunk());
     }
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent e) {
-        Location firstBlock = e.getChunk().getBlock(0, 100, 0).getLocation();
-        Island island = plugin.getGrid().getIslandAt(firstBlock);
-
-        if (island == null || island.isSpawn())
-            return;
-
-        if (e.getWorld().getEnvironment() == plugin.getSettings().getWorlds().getDefaultWorld()) {
-            island.setBiome(firstBlock.getWorld().getBiome(firstBlock.getBlockX(), firstBlock.getBlockZ()), false);
-        }
-
-        plugin.getNMSChunks().injectChunkSections(e.getChunk());
-
-        if (island.isInsideRange(e.getChunk()))
-            plugin.getNMSChunks().startTickingChunk(island, e.getChunk(), false);
-
-        if (!plugin.getNMSChunks().isChunkEmpty(e.getChunk()))
-            ChunksTracker.markDirty(island, e.getChunk(), true);
-
-        // We want to delete old holograms of stacked blocks
-        for (Entity entity : e.getChunk().getEntities()) {
-            if (entity instanceof ArmorStand && isHologram((ArmorStand) entity) &&
-                    plugin.getStackedBlocks().getStackedBlockAmount(entity.getLocation().subtract(0, 1, 0)) > 1)
-                entity.remove();
-        }
-
-        plugin.getStackedBlocks().updateStackedBlockHolograms(e.getChunk());
+        plugin.getWorldEventsManager().loadChunk(e.getChunk());
     }
 
     // Should potentially fix crop growth tile entities "disappearing"
@@ -79,11 +34,6 @@ public final class ChunksListener implements Listener {
         Island island = plugin.getGrid().getIslandAt(e.getBlock().getLocation());
         if (island != null && island.isInsideRange(e.getBlock().getLocation()))
             plugin.getNMSChunks().startTickingChunk(island, e.getBlock().getChunk(), false);
-    }
-
-    private static boolean isHologram(ArmorStand armorStand) {
-        return !armorStand.hasGravity() && armorStand.isSmall() && !armorStand.isVisible() &&
-                armorStand.isCustomNameVisible() && armorStand.isMarker() && armorStand.getCustomName() != null;
     }
 
 }
