e5052b08fb38ff3f5243f19dbefd4d6c70bb3a5f
OmerBenGera <omerthepro126@gmail.com>
2021-12-17 22:40:39 +0200

    Cleaned code

--- src/main/java/com/bgsoftware/superiorskyblock/utils/FileUtils.java
+++ src/main/java/com/bgsoftware/superiorskyblock/utils/FileUtils.java
@@ -29,7 +29,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
