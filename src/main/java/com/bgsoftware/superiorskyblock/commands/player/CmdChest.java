6c4c97c9022081fffe0c82a96658e44779030458
OmerBenGera <omerthepro126@gmail.com>
2022-01-05 14:04:06 +0200

    Added the ability to open chests using a param with `/is chest` (#819)

--- src/main/java/com/bgsoftware/superiorskyblock/commands/player/CmdChest.java
+++ src/main/java/com/bgsoftware/superiorskyblock/commands/player/CmdChest.java
@@ -1,15 +1,21 @@
 package com.bgsoftware.superiorskyblock.commands.player;
 
-import com.bgsoftware.superiorskyblock.lang.Message;
 import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.island.Island;
+import com.bgsoftware.superiorskyblock.api.island.IslandChest;
 import com.bgsoftware.superiorskyblock.api.island.IslandPrivilege;
+import com.bgsoftware.superiorskyblock.api.objects.Pair;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
+import com.bgsoftware.superiorskyblock.commands.CommandArguments;
+import com.bgsoftware.superiorskyblock.commands.CommandTabCompletes;
 import com.bgsoftware.superiorskyblock.commands.IPermissibleCommand;
 import com.bgsoftware.superiorskyblock.island.permissions.IslandPrivileges;
+import com.bgsoftware.superiorskyblock.lang.Message;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public final class CmdChest implements IPermissibleCommand {
 
@@ -25,7 +31,7 @@ public final class CmdChest implements IPermissibleCommand {
 
     @Override
     public String getUsage(java.util.Locale locale) {
-        return "chest";
+        return "chest [" + Message.COMMAND_ARGUMENT_PAGE.getMessage(locale) + "]";
     }
 
     @Override
@@ -40,7 +46,7 @@ public final class CmdChest implements IPermissibleCommand {
 
     @Override
     public int getMaxArgs() {
-        return 1;
+        return 2;
     }
 
     @Override
@@ -60,7 +66,32 @@ public final class CmdChest implements IPermissibleCommand {
 
     @Override
     public void execute(SuperiorSkyblockPlugin plugin, SuperiorPlayer superiorPlayer, Island island, String[] args) {
-        plugin.getMenus().openIslandChest(superiorPlayer, null, island);
+        if (args.length == 2) {
+            Pair<Integer, Boolean> pageArguments = CommandArguments.getPage(superiorPlayer.asPlayer(), args[1]);
+
+            if (!pageArguments.getValue())
+                return;
+
+            int page = pageArguments.getKey() - 1;
+            IslandChest[] islandChests = island.getChest();
+
+            if (page < 0 || page >= islandChests.length) {
+                Message.INVALID_PAGE.send(superiorPlayer, args[1]);
+                return;
+            }
+
+            islandChests[page].openChest(superiorPlayer);
+        } else {
+            plugin.getMenus().openIslandChest(superiorPlayer, null, island);
+        }
+    }
+
+    @Override
+    public List<String> tabComplete(SuperiorSkyblockPlugin plugin, SuperiorPlayer superiorPlayer, Island island, String[] args) {
+        IslandChest[] islandChests = island.getChest();
+        return args.length == 1 || islandChests.length == 0 ? new ArrayList<>() :
+                CommandTabCompletes.getCustomComplete(args[1], IntStream.range(1, islandChests.length + 1).boxed()
+                        .map(Object::toString).toArray(String[]::new));
     }
 
 }
