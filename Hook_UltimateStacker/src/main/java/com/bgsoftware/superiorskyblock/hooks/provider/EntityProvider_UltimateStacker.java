18ab27d16aed5b046e27b1c6183a02dfe0b546d8
OmerBenGera <omerthepro126@gmail.com>
2022-01-28 22:41:23 +0200

    Fixed a null issue with UltimateStacker when trying to get entity amounts

--- Hook_UltimateStacker/src/main/java/com/bgsoftware/superiorskyblock/hooks/provider/EntityProvider_UltimateStacker.java
+++ Hook_UltimateStacker/src/main/java/com/bgsoftware/superiorskyblock/hooks/provider/EntityProvider_UltimateStacker.java
@@ -2,6 +2,7 @@ package com.bgsoftware.superiorskyblock.hooks.provider;
 
 import com.bgsoftware.superiorskyblock.api.hooks.EntityProvider;
 import com.songoda.ultimatestacker.UltimateStacker;
+import com.songoda.ultimatestacker.stackable.entity.EntityStack;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
@@ -11,8 +12,12 @@ public final class EntityProvider_UltimateStacker implements EntityProvider {
 
     @Override
     public int getEntityAmount(Entity entity) {
-        return !(entity instanceof LivingEntity) ? 1 :
-                instance.getEntityStackManager().getStack((LivingEntity) entity).getAmount();
+        if (!(entity instanceof LivingEntity))
+            return 1;
+
+        EntityStack entityStack = instance.getEntityStackManager().getStack((LivingEntity) entity);
+
+        return entityStack == null ? 1 : entityStack.getAmount();
     }
 
 }
