cf7fb82dbc247b131332030d01d54f8310edbf2b
kingOf0 <ibrahimbugra1077@gmail.com>
2022-02-15 22:00:08 +0300

    Added PluginInitializedEvent

---
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/events/PluginInitializedEvent.java
@@ -0,0 +1,37 @@
+package com.bgsoftware.superiorskyblock.api.events;
+
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblock;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * PluginInitializedEvent is called when plugin enabled completely.
+ */
+public class PluginInitializedEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final SuperiorSkyblock plugin;
+
+    /**
+     * The constructor for the event.
+     * You cannot use handlers in this time, as none of them is set up.
+     */
+    public PluginInitializedEvent(SuperiorSkyblock plugin) {
+        this.plugin = plugin;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public SuperiorSkyblock getPlugin() {
+        return plugin;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+}
