d9075a62455249b4444a04a429ae595cba85f0e1
OmerBenGera <omerthepro126@gmail.com>
2022-06-03 12:18:25 +0300

    Fixed old versions of Oraxen not working (#1160)

--- Hook_Oraxen/src/main/java/com/bgsoftware/superiorskyblock/hooks/support/OraxenHook.java
+++ Hook_Oraxen/src/main/java/com/bgsoftware/superiorskyblock/hooks/support/OraxenHook.java
@@ -1,9 +1,9 @@
 package com.bgsoftware.superiorskyblock.hooks.support;
 
+import com.bgsoftware.common.reflection.ReflectMethod;
 import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
 import com.bgsoftware.superiorskyblock.api.events.IslandGenerateBlockEvent;
 import com.bgsoftware.superiorskyblock.api.objects.Pair;
-import com.google.common.collect.ImmutableList;
 import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
 import io.th0rgal.oraxen.mechanics.MechanicsManager;
@@ -16,16 +16,42 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
 public final class OraxenHook {
 
-    private static final List<Pair<MechanicFactory, SetBlockModelFunction>> AVAILABLE_MECHANICS = ImmutableList.of(
-            new Pair<>(MechanicsManager.getMechanicFactory("block"), BlockMechanicFactory::setBlockModel),
-            new Pair<>(MechanicsManager.getMechanicFactory("noteblock"), NoteBlockMechanicFactory::setBlockModel),
-            new Pair<>(MechanicsManager.getMechanicFactory("stringblock"), StringBlockMechanicFactory::setBlockModel)
-    );
+    private static final List<Pair<MechanicFactory, SetBlockModelFunction>> AVAILABLE_MECHANICS;
+
+    static {
+        List<Pair<MechanicFactory, SetBlockModelFunction>> availableMechanics = new ArrayList<>();
+
+        try {
+            Class.forName("io.th0rgal.oraxen.mechanics.provided.gameplay.block.BlockMechanicFactory");
+            availableMechanics.add(new Pair<>(MechanicsManager.getMechanicFactory("block"), BlockMechanicFactory::setBlockModel));
+            availableMechanics.add(new Pair<>(MechanicsManager.getMechanicFactory("noteblock"), NoteBlockMechanicFactory::setBlockModel));
+            availableMechanics.add(new Pair<>(MechanicsManager.getMechanicFactory("stringblock"), StringBlockMechanicFactory::setBlockModel));
+        } catch (Throwable error) {
+            availableMechanics.add(new Pair<>(MechanicsManager.getMechanicFactory("block"), (block, itemId) -> {
+                ReflectMethod<Void> setBlockModel = new ReflectMethod<>(
+                        "io.th0rgal.oraxen.mechanics.provided.block.BlockMechanicFactory",
+                        "setBlockModel", Block.class, String.class);
+                if (setBlockModel.isValid())
+                    setBlockModel.invoke(null, block, itemId);
+            }));
+            availableMechanics.add(new Pair<>(MechanicsManager.getMechanicFactory("noteblock"), (block, itemId) -> {
+                ReflectMethod<Void> setBlockModel = new ReflectMethod<>(
+                        "io.th0rgal.oraxen.mechanics.provided.noteblock.NoteBlockMechanicFactory",
+                        "setBlockModel", Block.class, String.class);
+                if (setBlockModel.isValid())
+                    setBlockModel.invoke(null, block, itemId);
+            }));
+        }
+
+        AVAILABLE_MECHANICS = Collections.unmodifiableList(availableMechanics);
+    }
 
     public static void register(SuperiorSkyblockPlugin plugin) {
         Bukkit.getPluginManager().registerEvents(new GeneratorListener(), plugin);
