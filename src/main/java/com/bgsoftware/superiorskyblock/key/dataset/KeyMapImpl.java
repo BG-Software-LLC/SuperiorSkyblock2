d921643c29bdf9767f52094a2c8239c34b45f11e
OmerBenGera <omerthepro126@gmail.com>
2022-06-03 15:32:47 +0300

    Fixed KeyMap iterators not reflecting the inner map

--- src/main/java/com/bgsoftware/superiorskyblock/key/dataset/KeyMapImpl.java
+++ src/main/java/com/bgsoftware/superiorskyblock/key/dataset/KeyMapImpl.java
@@ -6,11 +6,15 @@ import com.bgsoftware.superiorskyblock.key.KeyImpl;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.AbstractMap;
+import java.util.AbstractSet;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.Spliterator;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -21,6 +25,8 @@ public final class KeyMapImpl<V> extends AbstractMap<Key, V> implements KeyMap<V
     private static final KeyMapImpl EMPTY_MAP = new KeyMapImpl(() -> Collections.emptyMap());
 
     private final Map<String, V> innerMap;
+
+    private Set<Entry<Key, V>> entrySet;
 
     public static <V> KeyMapImpl<V> create(Supplier<Map<String, V>> mapCreator) {
         return new KeyMapImpl<>(mapCreator);
@@ -96,7 +102,7 @@ public final class KeyMapImpl<V> extends AbstractMap<Key, V> implements KeyMap<V
     @Override
     @NotNull
     public Set<Entry<Key, V>> entrySet() {
-        return asMap().entrySet();
+        return entrySet == null ? (entrySet = new EntrySet()) : entrySet;
     }
 
     @Override
@@ -141,6 +147,125 @@ public final class KeyMapImpl<V> extends AbstractMap<Key, V> implements KeyMap<V
         return innerMap.entrySet().stream().collect(Collectors.toMap(entry -> KeyImpl.of(entry.getKey()), Entry::getValue, (v1, v2) -> {
             throw new IllegalStateException(String.format("Duplicate key %s", v1));
         }, HashMap::new));
+    }
+
+    private final class EntrySet extends AbstractSet<Entry<Key, V>> {
+        public int size() {
+            return KeyMapImpl.this.size();
+        }
+
+        public void clear() {
+            KeyMapImpl.this.clear();
+        }
+
+        public @NotNull Iterator<Entry<Key, V>> iterator() {
+            return new EntryIterator();
+        }
+
+        public boolean contains(Object o) {
+            if (!(o instanceof Map.Entry))
+                return false;
+            Map.Entry<?, ?> e = (Map.Entry<?, ?>) o;
+            Object key = e.getKey();
+            V mapValue = KeyMapImpl.this.get(key);
+            return mapValue != null && mapValue.equals(e.getValue());
+        }
+
+        public boolean remove(Object o) {
+            if (o instanceof Map.Entry) {
+                Map.Entry<?, ?> e = (Map.Entry<?, ?>) o;
+                Object key = e.getKey();
+                Object value = e.getValue();
+                return KeyMapImpl.this.remove(key, value);
+            }
+            return false;
+        }
+
+        public Spliterator<Entry<Key, V>> spliterator() {
+            return new EntrySpliterator();
+        }
+
+        public void forEach(Consumer<? super Entry<Key, V>> action) {
+            iterator().forEachRemaining(action);
+        }
+    }
+
+    private final class EntryIterator implements Iterator<Entry<Key, V>> {
+
+        final Iterator<Entry<String, V>> innerMapIterator = KeyMapImpl.this.innerMap.entrySet().iterator();
+
+        @Override
+        public boolean hasNext() {
+            return innerMapIterator.hasNext();
+        }
+
+        @Override
+        public Entry<Key, V> next() {
+            return new KeyEntry(innerMapIterator.next());
+        }
+
+        @Override
+        public void remove() {
+            innerMapIterator.remove();
+        }
+    }
+
+    private final class EntrySpliterator implements Spliterator<Entry<Key, V>> {
+
+        final Spliterator<Entry<String, V>> spliterator;
+
+        EntrySpliterator() {
+            this(KeyMapImpl.this.innerMap.entrySet().spliterator());
+        }
+
+        EntrySpliterator(Spliterator<Entry<String, V>> spliterator) {
+            this.spliterator = spliterator;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super Entry<Key, V>> action) {
+            return spliterator.tryAdvance(entry -> action.accept(new KeyEntry(entry)));
+        }
+
+        @Override
+        public Spliterator<Entry<Key, V>> trySplit() {
+            return new EntrySpliterator(spliterator.trySplit());
+        }
+
+        @Override
+        public long estimateSize() {
+            return spliterator.estimateSize();
+        }
+
+        @Override
+        public int characteristics() {
+            return spliterator.characteristics();
+        }
+    }
+
+    private final class KeyEntry implements Entry<Key, V> {
+
+        private final Entry<String, V> entry;
+
+        KeyEntry(Entry<String, V> entry) {
+            this.entry = entry;
+        }
+
+        @Override
+        public Key getKey() {
+            return Key.of(entry.getKey());
+        }
+
+        @Override
+        public V getValue() {
+            return entry.getValue();
+        }
+
+        @Override
+        public V setValue(V value) {
+            return entry.setValue(value);
+        }
+
     }
 
 }
