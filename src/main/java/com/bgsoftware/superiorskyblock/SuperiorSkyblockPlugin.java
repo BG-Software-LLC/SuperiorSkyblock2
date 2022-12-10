0c994e0bad7d02b1d18f1b5477d538ca1cb33ff7
OmerBenGera <omerthepro126@gmail.com>
2022-12-10 14:15:09 +0200

    Added support for 1.19.3

--- src/main/java/com/bgsoftware/superiorskyblock/SuperiorSkyblockPlugin.java
+++ src/main/java/com/bgsoftware/superiorskyblock/SuperiorSkyblockPlugin.java
@@ -403,7 +403,8 @@ public class SuperiorSkyblockPlugin extends JavaPlugin implements SuperiorSkyblo
                     new Pair<>(2975, "v1182"),
                     new Pair<>(3105, "v119"),
                     new Pair<>(3117, "v1191"),
-                    new Pair<>(3120, "v1192")
+                    new Pair<>(3120, "v1192"),
+                    new Pair<>(3218, "v1193")
             );
 
             for (Pair<Integer, String> versionData : versions) {
