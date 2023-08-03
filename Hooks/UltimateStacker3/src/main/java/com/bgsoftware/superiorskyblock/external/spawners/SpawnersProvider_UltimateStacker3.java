a56c21b1781992cb8421ee6b8c7c1c4383c72536
ceze88 <dev.ceze@gmail.com>
2023-08-03 13:24:42 +0200

    Update hook for UltimateStacker

---
+++ Hooks/UltimateStacker3/src/main/java/com/bgsoftware/superiorskyblock/external/spawners/SpawnersProvider_UltimateStacker3.java
@@ -0,0 +1,83 @@
+package com.bgsoftware.superiorskyblock.external.spawners;
+
+import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
+import com.bgsoftware.superiorskyblock.api.hooks.SpawnersProvider;
+import com.bgsoftware.superiorskyblock.api.island.Island;
+import com.bgsoftware.superiorskyblock.api.key.Key;
+import com.bgsoftware.superiorskyblock.api.objects.Pair;
+import com.bgsoftware.superiorskyblock.core.Materials;
+import com.bgsoftware.superiorskyblock.core.formatting.Formatters;
+import com.bgsoftware.superiorskyblock.core.key.ConstantKeys;
+import com.bgsoftware.superiorskyblock.core.logging.Log;
+import com.bgsoftware.superiorskyblock.core.messages.Message;
+import com.bgsoftware.superiorskyblock.core.threads.BukkitExecutor;
+import com.craftaro.ultimatestacker.api.UltimateStackerApi;
+import com.craftaro.ultimatestacker.api.events.spawner.SpawnerBreakEvent;
+import com.craftaro.ultimatestacker.api.events.spawner.SpawnerPlaceEvent;
+import com.google.common.base.Preconditions;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+@SuppressWarnings("unused")
+public class SpawnersProvider_UltimateStacker3 implements SpawnersProviderItemMetaSpawnerType {
+
+    private static boolean registered = false;
+
+    private final SuperiorSkyblockPlugin plugin;
+
+    public SpawnersProvider_UltimateStacker3(SuperiorSkyblockPlugin plugin) {
+        this.plugin = plugin;
+        if (!registered) {
+            Bukkit.getPluginManager().registerEvents(new StackerListener(), plugin);
+            registered = true;
+            Log.info("Using UltimateStacker as a spawners provider.");
+        }
+    }
+
+    @Override
+    public Pair<Integer, String> getSpawner(Location location) {
+        Preconditions.checkNotNull(location, "location parameter cannot be null.");
+
+        int blockCount = -1;
+        if (Bukkit.isPrimaryThread()) {
+            blockCount = UltimateStackerApi.getSpawnerStackManager().getSpawner(location).getAmount();
+        }
+
+        return new Pair<>(blockCount, null);
+    }
+
+    @SuppressWarnings("unused")
+    private class StackerListener implements Listener {
+
+        @EventHandler(priority = EventPriority.HIGHEST)
+        public void onSpawnerStack(SpawnerPlaceEvent e) {
+            Island island = plugin.getGrid().getIslandAt(e.getBlock().getLocation());
+
+            if (island == null)
+                return;
+
+            Key blockKey = Key.of(Materials.SPAWNER.toBukkitType() + "", e.getSpawnerType().name());
+            int increaseAmount = e.getAmount();
+
+            if (island.hasReachedBlockLimit(blockKey, increaseAmount)) {
+                e.setCancelled(true);
+                Message.REACHED_BLOCK_LIMIT.send(e.getPlayer(), Formatters.CAPITALIZED_FORMATTER.format(blockKey.toString()));
+            } else {
+                island.handleBlockPlace(blockKey, increaseAmount);
+            }
+        }
+
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        public void onSpawnerUnstack(SpawnerBreakEvent e) {
+            Island island = plugin.getGrid().getIslandAt(e.getBlock().getLocation());
+            if (island != null)
+                island.handleBlockBreak(e.getBlock(), e.getAmount());
+        }
+
+    }
+
+}
