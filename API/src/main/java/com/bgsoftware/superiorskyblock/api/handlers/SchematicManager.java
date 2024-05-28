e3b7640c3bad029cceb869a7abc9926a0bd1131f
Martin R. O <me@snake.rip>
2024-05-28 14:39:54 -0300

    feat: Refactor saveSchematic method to use Location object for offset and spawn

--- API/src/main/java/com/bgsoftware/superiorskyblock/api/handlers/SchematicManager.java
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/handlers/SchematicManager.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface SchematicManager {
 
     /**
-     * Get a schematic by it's name.
+     * Get a schematic by its name.
      *
      * @param name The name to check.
      * @return The schematic with that name.
@@ -53,53 +53,22 @@ public interface SchematicManager {
      *
      * @param pos1          First position for the schematic.
      * @param pos2          Second position for the schematic.
-     * @param offsetX       The offset x value for the schematic (from minimum location between the two)
-     * @param offsetY       The offset y value for the schematic (from minimum location between the two)
-     * @param offsetZ       The offset z value for the schematic (from minimum location between the two)
+     * @param offset        The offset location for the schematic (from minimum location between the two)
+     * @param spawn         The spawn location for the schematic.
      * @param schematicName The new schematic name that will be created.
      */
-    void saveSchematic(Location pos1, Location pos2, int offsetX, int offsetY, int offsetZ, String schematicName);
+    void saveSchematic(Location pos1, Location pos2, Location offset, Location spawn, String schematicName);
 
     /**
      * Save a schematic.
      *
      * @param pos1          First position for the schematic.
      * @param pos2          Second position for the schematic.
-     * @param offsetX       The offset x value for the schematic (from minimum location between the two)
-     * @param offsetY       The offset y value for the schematic (from minimum location between the two)
-     * @param offsetZ       The offset z value for the schematic (from minimum location between the two)
-     * @param yaw           The yaw value of the schematic.
-     * @param pitch         The pitch value of the schematic.
+     * @param offset        The offset location for the schematic (from minimum location between the two)
+     * @param spawn         The spawn location for the schematic.
      * @param schematicName The new schematic name that will be created.
+     * @param callable      A runnable that will be run after the task is completed.
      */
-    void saveSchematic(Location pos1, Location pos2, int offsetX, int offsetY, int offsetZ, float yaw, float pitch, String schematicName);
-
-    /**
-     * Save a schematic.
-     *
-     * @param pos1          First position for the schematic.
-     * @param pos2          Second position for the schematic.
-     * @param offsetX       The offset x value for the schematic (from minimum location between the two)
-     * @param offsetY       The offset y value for the schematic (from minimum location between the two)
-     * @param offsetZ       The offset z value for the schematic (from minimum location between the two)
-     * @param schematicName The new schematic name that will be created.
-     * @param callable      A runnable that will be ran after the task is completed.
-     */
-    void saveSchematic(Location pos1, Location pos2, int offsetX, int offsetY, int offsetZ, String schematicName, Runnable callable);
-
-    /**
-     * Save a schematic.
-     *
-     * @param pos1          First position for the schematic.
-     * @param pos2          Second position for the schematic.
-     * @param offsetX       The offset x value for the schematic (from minimum location between the two)
-     * @param offsetY       The offset y value for the schematic (from minimum location between the two)
-     * @param offsetZ       The offset z value for the schematic (from minimum location between the two)
-     * @param yaw           The yaw value of the schematic.
-     * @param pitch         The pitch value of the schematic.
-     * @param schematicName The new schematic name that will be created.
-     * @param callable      A runnable that will be ran after the task is completed.
-     */
-    void saveSchematic(Location pos1, Location pos2, int offsetX, int offsetY, int offsetZ, float yaw, float pitch, String schematicName, @Nullable Runnable callable);
+    void saveSchematic(Location pos1, Location pos2, Location offset, Location spawn, String schematicName, @Nullable Runnable callable);
 
 }
