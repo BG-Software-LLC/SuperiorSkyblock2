e763d2818a644a08b62e7d2f9cc94fcc6ffd097b
OmerBenGera <omerthepro126@gmail.com>
2024-06-22 12:09:18 +0300

    Fix entity recalculation not checking if the entity can bypass entity limits (#1988)

--- API/src/main/java/com/bgsoftware/superiorskyblock/api/hooks/EntitiesProvider.java
+++ API/src/main/java/com/bgsoftware/superiorskyblock/api/hooks/EntitiesProvider.java
@@ -7,6 +7,9 @@ public interface EntitiesProvider {
     /**
      * Should the plugin track the entity {@param entity}?
      * This is relevant for spawning, de-spawning and entity limits.
+     * <p>
+     * Please note: The entity provided to this function may be in unloaded chunks.
+     * Do not attempt to change its attributes in this method in any way.
      *
      * @param entity The entity to check.
      */
