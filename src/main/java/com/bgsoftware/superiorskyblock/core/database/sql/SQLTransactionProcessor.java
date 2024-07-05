40f4205d872593d56646ff9307285a316f8d8678
OmerBenGera <omerthepro126@gmail.com>
2024-07-05 19:35:03 +0300

    Better handling of batch transactions

--- src/main/java/com/bgsoftware/superiorskyblock/core/database/sql/SQLTransactionProcessor.java
+++ src/main/java/com/bgsoftware/superiorskyblock/core/database/sql/SQLTransactionProcessor.java
@@ -1,14 +1,15 @@
 package com.bgsoftware.superiorskyblock.core.database.sql;
 
-import com.bgsoftware.superiorskyblock.core.database.transaction.IDatabaseTransaction;
 import com.bgsoftware.superiorskyblock.core.database.sql.session.QueryResult;
 import com.bgsoftware.superiorskyblock.core.database.sql.transaction.SQLDatabaseTransaction;
+import com.bgsoftware.superiorskyblock.core.database.transaction.IDatabaseTransaction;
 import com.bgsoftware.superiorskyblock.core.logging.Debug;
 import com.bgsoftware.superiorskyblock.core.logging.Log;
 import com.bgsoftware.superiorskyblock.core.mutable.MutableInt;
 import com.bgsoftware.superiorskyblock.core.mutable.MutableObject;
 import com.google.common.base.Preconditions;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -33,34 +34,78 @@ public class SQLTransactionProcessor {
         MutableObject<String> fullQuery = new MutableObject<>(query);
 
         executeQuery(query, new QueryResult<PreparedStatement>().onSuccess(preparedStatement -> {
-
             List<SQLDatabaseTransaction.DatabaseValues> allBatchValues = sqlTransaction.getValues();
-
-            for (SQLDatabaseTransaction.DatabaseValues batchValues : allBatchValues) {
-                fullQuery.setValue(query);
-                MutableInt index = new MutableInt(1);
-                for (Object value : batchValues.values) {
-                    addObject(preparedStatement, index, value, fullQuery);
-                }
-                Log.debug(Debug.DATABASE_QUERY, fullQuery.getValue());
-                preparedStatement.addBatch();
+            if (allBatchValues.size() > 1) {
+                executeBatchTransaction(preparedStatement, allBatchValues, query);
+            } else if (allBatchValues.size() == 1) {
+                executeTransaction(preparedStatement, allBatchValues.iterator().next(), query);
             }
-
-            preparedStatement.executeBatch();
         }).onFail(error -> {
             Log.error(error, "An unexpected error occurred while executing query `", fullQuery.getValue(), "`:");
         }));
     }
 
-    private void addObject(PreparedStatement preparedStatement, MutableInt index, Object value,
-                           MutableObject<String> fullQuery) throws SQLException {
+    private static void executeBatchTransaction(PreparedStatement preparedStatement,
+                                                List<SQLDatabaseTransaction.DatabaseValues> allBatchValues,
+                                                String query) throws SQLException {
+        MutableObject<String> fullQuery = new MutableObject<>(query);
+
+        Connection connection = preparedStatement.getConnection();
+
+        try {
+            connection.setAutoCommit(false);
+
+            for (SQLDatabaseTransaction.DatabaseValues batchValues : allBatchValues) {
+                fullQuery.setValue(query);
+
+                populateStatement(preparedStatement, batchValues, fullQuery);
+
+                Log.debug(Debug.DATABASE_QUERY, fullQuery.getValue());
+
+                preparedStatement.addBatch();
+            }
+
+            preparedStatement.executeBatch();
+
+            try {
+                connection.commit();
+            } catch (Throwable ignored) {
+            }
+        } finally {
+            connection.setAutoCommit(true);
+        }
+    }
+
+    private static void executeTransaction(PreparedStatement preparedStatement,
+                                           SQLDatabaseTransaction.DatabaseValues values,
+                                           String query) throws SQLException {
+        MutableObject<String> fullQuery = new MutableObject<>(query);
+
+        populateStatement(preparedStatement, values, fullQuery);
+
+        Log.debug(Debug.DATABASE_QUERY, fullQuery.getValue());
+
+        preparedStatement.executeUpdate();
+    }
+
+    private static void populateStatement(PreparedStatement preparedStatement,
+                                          SQLDatabaseTransaction.DatabaseValues values,
+                                          MutableObject<String> fullQuery) throws SQLException {
+        MutableInt index = new MutableInt(1);
+        for (Object value : values.values) {
+            addObject(preparedStatement, index, value, fullQuery);
+        }
+    }
+
+    private static void addObject(PreparedStatement preparedStatement, MutableInt index, Object value,
+                                  MutableObject<String> fullQuery) throws SQLException {
         int curr = index.get();
         preparedStatement.setObject(curr, value);
         index.set(curr + 1);
         fullQuery.setValue(QUERY_VALUE_PATTERN.matcher(fullQuery.getValue()).replaceFirst(value + ""));
     }
 
-    private void executeQuery(String query, QueryResult<PreparedStatement> callback) {
+    private static void executeQuery(String query, QueryResult<PreparedStatement> callback) {
         SQLHelper.waitForConnection();
         SQLHelper.customQuery(query, callback);
     }
