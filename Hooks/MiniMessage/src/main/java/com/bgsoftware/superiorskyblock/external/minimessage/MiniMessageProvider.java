614549518d2a567019ac16abd248415d98e2f7f5
Maxlego08 <maxlego08@sfr.fr>
2024-07-21 16:35:45 +0200

    :construction: Add minimessage format

---
+++ Hooks/MiniMessage/src/main/java/com/bgsoftware/superiorskyblock/external/minimessage/MiniMessageProvider.java
@@ -0,0 +1,72 @@
+package com.bgsoftware.superiorskyblock.external.minimessage;
+
+import com.bgsoftware.superiorskyblock.api.service.message.MessageProvider;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MiniMessageProvider implements MessageProvider {
+
+    private final Map<String, String> COLORS_MAPPINGS = new HashMap<>();
+    private final MiniMessage minimessage = MiniMessage.builder().tags(StandardTags.defaults()).build();
+
+    public MiniMessageProvider() {
+        this.COLORS_MAPPINGS.put("0", "black");
+        this.COLORS_MAPPINGS.put("1", "dark_blue");
+        this.COLORS_MAPPINGS.put("2", "dark_green");
+        this.COLORS_MAPPINGS.put("3", "dark_aqua");
+        this.COLORS_MAPPINGS.put("4", "dark_red");
+        this.COLORS_MAPPINGS.put("5", "dark_purple");
+        this.COLORS_MAPPINGS.put("6", "gold");
+        this.COLORS_MAPPINGS.put("7", "gray");
+        this.COLORS_MAPPINGS.put("8", "dark_gray");
+        this.COLORS_MAPPINGS.put("9", "blue");
+        this.COLORS_MAPPINGS.put("a", "green");
+        this.COLORS_MAPPINGS.put("b", "aqua");
+        this.COLORS_MAPPINGS.put("c", "red");
+        this.COLORS_MAPPINGS.put("d", "light_purple");
+        this.COLORS_MAPPINGS.put("e", "yellow");
+        this.COLORS_MAPPINGS.put("f", "white");
+        this.COLORS_MAPPINGS.put("k", "obfuscated");
+        this.COLORS_MAPPINGS.put("l", "bold");
+        this.COLORS_MAPPINGS.put("m", "strikethrough");
+        this.COLORS_MAPPINGS.put("n", "underlined");
+        this.COLORS_MAPPINGS.put("o", "italic");
+        this.COLORS_MAPPINGS.put("r", "reset");
+    }
+
+    @Override
+    public void sendMessage(Player player, String message) {
+        player.sendMessage(minimessage.deserialize(colorMiniMessage(message)));
+    }
+
+    private String colorMiniMessage(String message) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Pattern pattern = Pattern.compile("(?<!<)(?<!:)#([a-fA-F0-9]{6})");
+        Matcher matcher = pattern.matcher(message);
+
+        while (matcher.find()) {
+            matcher.appendReplacement(stringBuilder, "<$0>");
+        }
+        matcher.appendTail(stringBuilder);
+
+        String newMessage = stringBuilder.toString();
+
+        for (Map.Entry<String, String> entry : this.COLORS_MAPPINGS.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            newMessage = newMessage.replace("&" + key, "<" + value + ">");
+            newMessage = newMessage.replace("§" + key, "<" + value + ">");
+            newMessage = newMessage.replace("&" + key.toUpperCase(), "<" + value + ">");
+            newMessage = newMessage.replace("§" + key.toUpperCase(), "<" + value + ">");
+        }
+
+        return newMessage;
+    }
+}
