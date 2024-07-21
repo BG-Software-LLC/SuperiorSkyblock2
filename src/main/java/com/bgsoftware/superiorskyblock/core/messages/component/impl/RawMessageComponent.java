d3629cde3eca8e6a792e91bf68e1d43015be81e0
Maxlego08 <maxlego08@sfr.fr>
2024-07-21 16:35:45 +0200

    :construction: Add minimessage format

--- src/main/java/com/bgsoftware/superiorskyblock/core/messages/component/impl/RawMessageComponent.java
+++ src/main/java/com/bgsoftware/superiorskyblock/core/messages/component/impl/RawMessageComponent.java
@@ -2,13 +2,31 @@ package com.bgsoftware.superiorskyblock.core.messages.component.impl;
 
 import com.bgsoftware.common.annotations.Nullable;
 import com.bgsoftware.superiorskyblock.api.service.message.IMessageComponent;
+import com.bgsoftware.superiorskyblock.api.service.message.MessageProvider;
 import com.bgsoftware.superiorskyblock.core.Text;
 import com.bgsoftware.superiorskyblock.core.messages.MessageContent;
 import com.bgsoftware.superiorskyblock.core.messages.component.EmptyMessageComponent;
+import com.bgsoftware.superiorskyblock.service.message.SpigotMessageProvider;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class RawMessageComponent implements IMessageComponent {
 
+    private final String message;
+    private MessageProvider messageProvider;
+
+    private RawMessageComponent(String message) {
+        this.message = message;
+        try {
+            Class.forName("net.kyori.adventure.text.minimessage.MiniMessage");
+            this.messageProvider = (MessageProvider) Class.forName("com.bgsoftware.superiorskyblock.external.minimessage.MiniMessageProvider").getConstructor().newInstance();
+        } catch (Exception ignored) {
+            this.messageProvider = new SpigotMessageProvider();
+        }
+    }
+
+    public static IMessageComponent of(@Nullable String message) {
+        return Text.isBlank(message) ? EmptyMessageComponent.getInstance() : new RawMessageComponent(message);
     private final MessageContent content;
 
     public static IMessageComponent of(@Nullable String message) {
