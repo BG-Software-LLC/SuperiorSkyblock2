1efe7136bd0cb2d576c69f0c0aa9cff38110e1a6
OmerBenGera <omerthepro126@gmail.com>
2024-09-18 00:07:47 +0300

    Add simple stats collector for profilers

---
+++ src/main/java/com/bgsoftware/superiorskyblock/core/profiler/ProfilerSession.java
@@ -0,0 +1,70 @@
+package com.bgsoftware.superiorskyblock.core.profiler;
+
+import com.bgsoftware.superiorskyblock.SuperiorSkyblockPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ProfilerSession {
+
+    private static final SuperiorSkyblockPlugin plugin = SuperiorSkyblockPlugin.getPlugin();
+
+    private final long id;
+    private final AtomicInteger stopCount;
+    private final ProfileType profileType;
+    private final Data startData;
+    private Data endData;
+
+    public ProfilerSession(long id, int stopCount, ProfileType profileType) {
+        this.id = id;
+        this.stopCount = new AtomicInteger(stopCount);
+        this.profileType = profileType;
+        this.startData = new Data();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public ProfileType getProfileType() {
+        return profileType;
+    }
+
+    public Data getStartData() {
+        return startData;
+    }
+
+    public Data getEndData() {
+        return Objects.requireNonNull(endData);
+    }
+
+    public boolean end() {
+        boolean ended = this.stopCount.decrementAndGet() <= 0;
+        if (ended)
+            this.endData = new Data();
+        return ended;
+    }
+
+    public String[] dump() {
+        List<String> dump = new ArrayList<>();
+
+        dump.add("Profiler #" + this.id);
+        dump.add("  Type: " + this.profileType.getPrettyName());
+        dump.add("  Time elapsed: " + TimeUnit.NANOSECONDS.toMillis(this.endData.time - this.startData.time) + "ms");
+        dump.add("  TPS: " + this.startData.tps + " -> " + this.endData.tps);
+
+        return dump.toArray(new String[0]);
+    }
+
+    public static class Data {
+
+
+        public final long time = System.nanoTime();
+        public final double tps = plugin.getNMSAlgorithms().getCurrentTps();
+
+    }
+
+}
---
+++ src/main/java/com/bgsoftware/superiorskyblock/core/stats/StatsClient.java
@@ -0,0 +1,75 @@
+package com.bgsoftware.superiorskyblock.core.stats;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.google.gson.JsonObject;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class StatsClient {
+
+    private static final int VERSION = 1;
+    private static final long TASK_DELAY = 1;
+    private static final List<IStatsCollector> STATS_COLLECTORS = new LinkedList<>();
+
+    private static StatsClient INSTANCE;
+
+    static {
+        STATS_COLLECTORS.add(StatsProfilers.INSTANCE);
+    }
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1, new ThreadFactoryBuilder()
+            .setNameFormat("SuperiorSkyblock Stats").build());
+
+    private ScheduledFuture<?> scheduledTask;
+
+    private StatsClient() {
+
+    }
+
+    public void start() {
+        this.scheduledTask = scheduler.scheduleAtFixedRate(this::collectStatsTask, 0, TASK_DELAY, TimeUnit.HOURS);
+    }
+
+    public void shutdown() {
+        if (this.scheduledTask != null) {
+            this.scheduledTask.cancel(true);
+            this.scheduledTask = null;
+        }
+
+        if (!this.scheduler.isShutdown())
+            this.scheduler.shutdownNow();
+    }
+
+    private void collectStatsTask() {
+        JsonObject statsObject = new JsonObject();
+        STATS_COLLECTORS.forEach(collector -> collector.collect(statsObject));
+        submitStats(statsObject);
+    }
+
+    private void submitStats(JsonObject statsObject) {
+        if (statsObject.entrySet().isEmpty())
+            return;
+
+        statsObject.addProperty("version", VERSION);
+
+        // TODO: Submit to API
+    }
+
+    public static StatsClient getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new StatsClient();
+
+        return INSTANCE;
+    }
+
+    public static Optional<StatsClient> getIfExists() {
+        return Optional.ofNullable(INSTANCE);
+    }
+
+}
