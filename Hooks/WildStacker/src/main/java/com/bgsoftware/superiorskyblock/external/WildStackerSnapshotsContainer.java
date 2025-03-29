32eb6735a598132ddf6cf70375e1b46b7b1acd85
OmerBenGera <omerthepro126@gmail.com>
2025-03-29 15:54:46 +0300

    Potential fix for chunk snapshots not cached error with WildStacker (#2366)

--- Hooks/WildStacker/src/main/java/com/bgsoftware/superiorskyblock/external/WildStackerSnapshotsContainer.java
+++ Hooks/WildStacker/src/main/java/com/bgsoftware/superiorskyblock/external/WildStackerSnapshotsContainer.java
@@ -1,25 +1,27 @@
 package com.bgsoftware.superiorskyblock.external;
 
 import com.bgsoftware.superiorskyblock.core.ChunkPosition;
+import com.bgsoftware.superiorskyblock.core.collections.Chunk2ObjectMap;
 import com.bgsoftware.superiorskyblock.core.logging.Log;
 import com.bgsoftware.wildstacker.api.WildStackerAPI;
 import com.bgsoftware.wildstacker.api.objects.StackedSnapshot;
 import org.bukkit.Chunk;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 public class WildStackerSnapshotsContainer {
 
-    private static final Map<ChunkPosition, StackedSnapshot> cachedSnapshots = new ConcurrentHashMap<>();
+    private static final Chunk2ObjectMap<StackedSnapshot> cachedSnapshots = new Chunk2ObjectMap<>();
 
     private WildStackerSnapshotsContainer() {
 
     }
 
     public static void takeSnapshot(Chunk chunk) {
-        ChunkPosition chunkPosition = ChunkPosition.of(chunk, false);
+        try (ChunkPosition chunkPosition = ChunkPosition.of(chunk)) {
+            takeSnapshotInternal(chunk, chunkPosition);
+        }
+    }
 
+    private static void takeSnapshotInternal(Chunk chunk, ChunkPosition chunkPosition) {
         if (cachedSnapshots.containsKey(chunkPosition))
             return;
 
