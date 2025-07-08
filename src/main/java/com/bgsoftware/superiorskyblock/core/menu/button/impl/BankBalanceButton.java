99a88d1b49c7fb1b78db9901dacc525ed7b7e96a
Lemvs <48791834+Lemvs@users.noreply.github.com>
2025-07-08 13:28:12 +0200

    Added balance button to not require using PlaceholderAPI to display bank balance

---
+++ src/main/java/com/bgsoftware/superiorskyblock/core/menu/button/impl/BankBalanceButton.java
@@ -0,0 +1,56 @@
+package com.bgsoftware.superiorskyblock.core.menu.button.impl;
+
+import com.bgsoftware.superiorskyblock.api.island.Island;
+import com.bgsoftware.superiorskyblock.api.menu.button.MenuTemplateButton;
+import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
+import com.bgsoftware.superiorskyblock.core.formatting.Formatters;
+import com.bgsoftware.superiorskyblock.core.itemstack.ItemBuilder;
+import com.bgsoftware.superiorskyblock.core.menu.button.AbstractMenuTemplateButton;
+import com.bgsoftware.superiorskyblock.core.menu.button.AbstractMenuViewButton;
+import com.bgsoftware.superiorskyblock.core.menu.button.MenuTemplateButtonImpl;
+import com.bgsoftware.superiorskyblock.core.menu.view.impl.IslandMenuView;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.time.Duration;
+
+public class BankBalanceButton extends AbstractMenuViewButton<IslandMenuView> {
+
+    private BankBalanceButton(AbstractMenuTemplateButton<IslandMenuView> templateButton, IslandMenuView menuView) {
+        super(templateButton, menuView);
+    }
+
+    @Override
+    public ItemStack createViewItem() {
+        Island island = menuView.getInventoryViewer().getIsland();
+        SuperiorPlayer inventoryViewer = menuView.getInventoryViewer();
+        ItemStack buttonItem = super.createViewItem();
+
+        return new ItemBuilder(buttonItem)
+                .replaceAll("{0}", island.getIslandBank().getBalance() + "")
+                .replaceAll("{1}", Formatters.NUMBER_FORMATTER.format(island.getIslandBank().getBalance()))
+                .replaceAll("{2}", Formatters.FANCY_NUMBER_FORMATTER.format(island.getIslandBank().getBalance(), inventoryViewer.getUserLocale()))
+                .replaceAll("{3}", island.getBankLimit() + "")
+                .replaceAll("{4}", Formatters.NUMBER_FORMATTER.format(island.getBankLimit()))
+                .replaceAll("{5}", Formatters.FANCY_NUMBER_FORMATTER.format(island.getBankLimit(), inventoryViewer.getUserLocale()))
+                .replaceAll("{6}", Formatters.TIME_FORMATTER.format(Duration.ofSeconds(island.getLastInterestTime()), inventoryViewer.getUserLocale()))
+                .replaceAll("{7}", Formatters.TIME_FORMATTER.format(Duration.ofSeconds(island.getNextInterest()), inventoryViewer.getUserLocale()))
+                .build();
+    }
+
+    @Override
+    public void onButtonClick(InventoryClickEvent clickEvent) {
+        // Dummy button
+    }
+
+    public static class Builder extends AbstractMenuTemplateButton.AbstractBuilder<IslandMenuView> {
+
+        @Override
+        public MenuTemplateButton<IslandMenuView> build() {
+            return new MenuTemplateButtonImpl<>(buttonItem, clickSound, commands, requiredPermission,
+                    lackPermissionSound, BankBalanceButton.class, BankBalanceButton::new);
+        }
+
+    }
+
+}
