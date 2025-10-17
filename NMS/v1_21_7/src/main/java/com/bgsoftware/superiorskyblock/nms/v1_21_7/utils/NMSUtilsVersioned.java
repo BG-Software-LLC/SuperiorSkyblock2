2e98cc8d1bf6170ea9813fb6140dd7141637434c
Lemvs <48791834+Lemvs@users.noreply.github.com>
2025-10-17 09:01:19 +0200

    Fixed creating schematics with signs on 1.21.7+ (v2) (#2722)

--- NMS/v1_21_7/src/main/java/com/bgsoftware/superiorskyblock/nms/v1_21_7/utils/NMSUtilsVersioned.java
+++ NMS/v1_21_7/src/main/java/com/bgsoftware/superiorskyblock/nms/v1_21_7/utils/NMSUtilsVersioned.java
@@ -352,8 +352,10 @@ public class NMSUtilsVersioned {
                 }
             }
 
-            while (textLines.size() < 4)
-                textLines.add(Component.empty());
+            for (int i = 0; i < 4; i++) {
+                if (textLines.get(i) == null)
+                    textLines.set(i, Component.empty());
+            }
 
             Component[] textLinesArray = textLines.toArray(COMPONENT_ARRAY_TYPE);
 
