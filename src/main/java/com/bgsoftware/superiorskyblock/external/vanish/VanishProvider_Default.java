914184bfe53f89b7d66caf4b2e80cad9c67da83c
nivcoo <nico64800@live.fr>
2025-10-30 13:38:26 +0100

    improv. fix vanished check

--- src/main/java/com/bgsoftware/superiorskyblock/external/vanish/VanishProvider_Default.java
+++ src/main/java/com/bgsoftware/superiorskyblock/external/vanish/VanishProvider_Default.java
@@ -2,11 +2,16 @@ package com.bgsoftware.superiorskyblock.external.vanish;
 
 import com.bgsoftware.superiorskyblock.api.hooks.VanishProvider;
 import org.bukkit.entity.Player;
+import org.bukkit.metadata.MetadataValue;
 
 public class VanishProvider_Default implements VanishProvider {
 
     @Override
     public boolean isVanished(Player player) {
-        return player.hasMetadata("vanished");
+        if (!player.hasMetadata("vanished"))
+            return false;
+
+        return player.getMetadata("vanished").stream()
+                .anyMatch(MetadataValue::asBoolean);
     }
 }
