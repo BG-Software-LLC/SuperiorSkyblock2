956b0932dfc4f5e5806c9c42b5a84f6e91cfe76e
nivcoo <nico64800@live.fr>
2025-10-30 15:00:38 +0100

    improv. add more variables on the global warps menu

--- src/main/java/com/bgsoftware/superiorskyblock/core/menu/button/impl/GlobalWarpsPagedObjectButton.java
+++ src/main/java/com/bgsoftware/superiorskyblock/core/menu/button/impl/GlobalWarpsPagedObjectButton.java
@@ -3,6 +3,8 @@ package com.bgsoftware.superiorskyblock.core.menu.button.impl;
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.menu.button.MenuTemplateButton;
 import com.bgsoftware.superiorskyblock.api.menu.button.PagedMenuTemplateButton;
+import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
+import com.bgsoftware.superiorskyblock.core.formatting.Formatters;
 import com.bgsoftware.superiorskyblock.core.itemstack.ItemBuilder;
 import com.bgsoftware.superiorskyblock.core.menu.Menus;
 import com.bgsoftware.superiorskyblock.core.menu.button.AbstractPagedMenuButton;
@@ -11,6 +13,9 @@ import com.bgsoftware.superiorskyblock.core.menu.impl.MenuGlobalWarps;
 import com.bgsoftware.superiorskyblock.core.menu.view.MenuViewWrapper;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Locale;
+import java.util.Map;
 
 public class GlobalWarpsPagedObjectButton extends AbstractPagedMenuButton<MenuGlobalWarps.View, Island> {
 
@@ -32,11 +37,42 @@ public class GlobalWarpsPagedObjectButton extends AbstractPagedMenuButton<MenuGl
 
     @Override
     public ItemStack modifyViewItem(ItemStack buttonItem) {
+        final SuperiorPlayer viewer = menuView.getInventoryViewer();
+        final SuperiorPlayer owner = pagedObject.getOwner();
+        final String ownerName = owner != null ? owner.getName() : "Unknown";
+        String islandName = pagedObject.getName();
+        if (islandName == null || islandName.isEmpty()) islandName = ownerName;
+
+        final int warpsCount = pagedObject.getIslandWarps().size();
+
+        Locale userLocale = viewer.getUserLocale();
+        String levelNum = Formatters.NUMBER_FORMATTER.format(pagedObject.getIslandLevel());
+        String worthNum = Formatters.NUMBER_FORMATTER.format(pagedObject.getWorth());
+        String levelFancy = Formatters.FANCY_NUMBER_FORMATTER.format(pagedObject.getIslandLevel(), userLocale);
+        String worthFancy = Formatters.FANCY_NUMBER_FORMATTER.format(pagedObject.getWorth(), userLocale);
+
+        int ratingCount = pagedObject.getRatingAmount();
+
+        String ratingAvgNum = Formatters.NUMBER_FORMATTER.format(pagedObject.getTotalRating());
+        String ratingAvgFancy = Formatters.RATING_FORMATTER.format(pagedObject.getTotalRating(), userLocale);
+
+        String[] descLines = (pagedObject.getDescription() == null || pagedObject.getDescription().isEmpty())
+                ? new String[0]
+                : pagedObject.getDescription().split("\n");
+
         return new ItemBuilder(buttonItem)
                 .asSkullOf(pagedObject.getOwner())
-                .replaceAll("{0}", pagedObject.getOwner().getName())
-                .replaceLoreWithLines("{1}", pagedObject.getDescription().split("\n"))
-                .replaceAll("{2}", pagedObject.getIslandWarps().size() + "")
+                .replaceAll("{0}", ownerName)
+                .replaceLoreWithLines("{1}", descLines)
+                .replaceAll("{2}", String.valueOf(warpsCount))
+                .replaceAll("{3}", islandName)
+                .replaceAll("{4}", levelNum)
+                .replaceAll("{5}", levelFancy)
+                .replaceAll("{6}", worthNum)
+                .replaceAll("{7}", worthFancy)
+                .replaceAll("{8}", ratingAvgNum)
+                .replaceAll("{9}", ratingAvgFancy)
+                .replaceAll("{10}", String.valueOf(ratingCount))
                 .build(pagedObject.getOwner());
     }
 
