d3191bb7cc765c920b9d03b9b0b418b0e02a69bc
Lemvs <48791834+Lemvs@users.noreply.github.com>
2025-12-06 16:54:57 +0100

    Fixed placing Bamboo Rafts without permissions

--- src/main/java/com/bgsoftware/superiorskyblock/core/Materials.java
+++ src/main/java/com/bgsoftware/superiorskyblock/core/Materials.java
@@ -162,7 +162,7 @@ public enum Materials {
                     material == Material.TRAPPED_CHEST || materialName.contains("SHULKER_BOX") ||
                     materialName.equals("BARREL"))
                 materialTags.add(Tag.CHEST);
-            if (materialName.contains("BOAT"))
+            if (materialName.contains("BOAT") || materialName.contains("_RAFT"))
                 materialTags.add(Tag.BOAT);
             if (materialName.contains("LAVA"))
                 materialTags.add(Tag.LAVA);
