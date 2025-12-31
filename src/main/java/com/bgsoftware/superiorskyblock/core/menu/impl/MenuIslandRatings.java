d7c442ba8568cf0ff1bec87e46df0149c9cbeb5c
Lemvs <48791834+Lemvs@users.noreply.github.com>
2025-12-31 15:59:14 +0100

    Fix ratings formatting in the ratings menu title

--- src/main/java/com/bgsoftware/superiorskyblock/core/menu/impl/MenuIslandRatings.java
+++ src/main/java/com/bgsoftware/superiorskyblock/core/menu/impl/MenuIslandRatings.java
@@ -8,6 +8,7 @@ import com.bgsoftware.superiorskyblock.api.menu.Menu;
 import com.bgsoftware.superiorskyblock.api.menu.view.MenuView;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import com.bgsoftware.superiorskyblock.core.SequentialListBuilder;
+import com.bgsoftware.superiorskyblock.core.formatting.Formatters;
 import com.bgsoftware.superiorskyblock.core.io.MenuParserImpl;
 import com.bgsoftware.superiorskyblock.core.menu.AbstractPagedMenu;
 import com.bgsoftware.superiorskyblock.core.menu.MenuIdentifiers;
@@ -71,7 +72,7 @@ public class MenuIslandRatings extends AbstractPagedMenu<MenuIslandRatings.View,
 
         @Override
         public String replaceTitle(String title) {
-            return title.replace("{0}", String.valueOf(island.getTotalRating()));
+            return title.replace("{0}", Formatters.NUMBER_FORMATTER.format(island.getTotalRating()));
         }
 
         @Override
