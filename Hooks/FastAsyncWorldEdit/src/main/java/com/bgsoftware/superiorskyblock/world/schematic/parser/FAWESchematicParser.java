78a958d37d5a244060fb24304ab5bb04ef6795a9
BaByCrash <babycrash.dev@gmail.com>
2026-06-04 09:47:56 +0200

    feat(Shematics): FAWE .schem integration
    - ajout de la prise en charge des shematic .schem

--- Hooks/FastAsyncWorldEdit/src/main/java/com/bgsoftware/superiorskyblock/world/schematic/parser/FAWESchematicParser.java
+++ Hooks/FastAsyncWorldEdit/src/main/java/com/bgsoftware/superiorskyblock/world/schematic/parser/FAWESchematicParser.java
@@ -3,9 +3,12 @@ package com.bgsoftware.superiorskyblock.world.schematic.parser;
 import com.bgsoftware.superiorskyblock.api.schematic.Schematic;
 import com.bgsoftware.superiorskyblock.api.schematic.parser.SchematicParseException;
 import com.bgsoftware.superiorskyblock.api.schematic.parser.SchematicParser;
+import com.bgsoftware.superiorskyblock.core.io.IOUtils;
 import com.bgsoftware.superiorskyblock.world.schematic.impl.WorldEditSchematic;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 
@@ -18,8 +21,23 @@ public class FAWESchematicParser implements SchematicParser {
     @Override
     public Schematic parseSchematic(DataInputStream inputStream, String schematicName) throws SchematicParseException {
         try {
-            //noinspection deprecation
-            return new WorldEditSchematic(schematicName, ClipboardFormat.SCHEMATIC.load(inputStream));
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+
+            // Try Sponge format first (for .schem files)
+            ClipboardFormat spongeFormat = ClipboardFormats.findByAlias("sponge");
+            if (spongeFormat != null) {
+                try (DataInputStream stream = new DataInputStream(new ByteArrayInputStream(bytes))) {
+                    //noinspection deprecation
+                    return new WorldEditSchematic(schematicName, spongeFormat.load(stream));
+                } catch (Exception ignored) {
+                }
+            }
+
+            // Fallback to legacy SCHEMATIC format
+            try (DataInputStream stream = new DataInputStream(new ByteArrayInputStream(bytes))) {
+                //noinspection deprecation
+                return new WorldEditSchematic(schematicName, ClipboardFormat.SCHEMATIC.load(stream));
+            }
         } catch (IOException error) {
             throw new SchematicParseException(schematicName + " is not a FastAsyncWorldEdit schematic.");
         }
